4c585fb4f2d234980a69ea56b8a3014d3c48630b
kazaneshimizu <ks@rocketa.co.jp>
2022-12-10 10:18:39 +0900

    feat: add first user page

---
+++ src/admin/components/views/CreateFirstUser/index.tsx
@@ -0,0 +1,41 @@
+import { Box, Button } from '@mui/material';
+import React from 'react';
+
+const RegisterAdmin: React.FC = () => {
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        alignItems: 'left',
+        p: 3,
+      }}
+    >
+      <p>Welcome to Superfast</p>
+      <div>
+        <p>名前（姓）</p>
+        <input type="text" required />
+      </div>
+      <div>
+        <p>名前（名）</p>
+        <input type="text" required />
+      </div>
+      <div>
+        <p>メールアドレス</p>
+        <input type="text" required />
+      </div>
+      <div>
+        <p>パスワード</p>
+        <input type="text" required />
+      </div>
+      <div>
+        <p>パスワード（確認）</p>
+        <input type="text" required />
+      </div>
+      <Button variant="contained" sx={{ mt: 2 }}>
+        登録
+      </Button>
+    </Box>
+  );
+};
+
+export default RegisterAdmin;
