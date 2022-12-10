89916e6d5e1206db2a9872a4dcf2ecabf086763a
kazaneshimizu <ks@rocketa.co.jp>
2022-12-11 08:25:22 +0900

    feat: change logo fill

--- src/admin/components/elements/Logo/index.tsx
+++ src/admin/components/elements/Logo/index.tsx
@@ -1,13 +1,10 @@
 import RenderCustomComponent from '@admin/components/utilities/RenderCustomComponent';
+import { useTheme } from '@mui/material';
 import React from 'react';
 
-const css = `
-  .graphic-logo path {
-    fill: #000;
-  }
-`;
-
 const DefaultLogo: React.FC = () => {
+  const theme = useTheme();
+
   return (
     <svg
       width="90"
@@ -17,7 +14,13 @@ const DefaultLogo: React.FC = () => {
       xmlns="http://www.w3.org/2000/svg"
       className="graphic-logo"
     >
-      <style>{css}</style>
+      <style>
+        {`
+          .graphic-logo path {
+            fill: ${theme.palette.text.primary};
+          }
+        `}
+      </style>
       <path d="M19.1531 0L42.049 13.8019V39.9956L24.8049 50V23.8064L1.89795 10.0155L19.1531 0Z" />
       <path d="M17.4097 48.5454V28.1035L0.132324 38.1301L17.4097 48.5454Z" />
     </svg>
