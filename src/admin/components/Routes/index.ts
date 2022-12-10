0998dfe1ce930395169a1203644c8d1d38acd13c
kazaneshimizu <ks@rocketa.co.jp>
2022-12-10 10:19:26 +0900

    feat: add routes create first user

--- src/admin/components/Routes/index.ts
+++ src/admin/components/Routes/index.ts
@@ -1,6 +1,7 @@
 import { useRoutes } from 'react-router-dom';
+import AuthRoutes from './AuthRoutes';
 import MainRoutes from './MainRoutes';
 
 export default function Routes() {
-  return useRoutes([MainRoutes]);
+  return useRoutes([MainRoutes, AuthRoutes]);
 }
