72eed2f3bf9f0b1c399756430b96b8c1acb7b99a
kazaneshimizu <ks@rocketa.co.jp>
2022-12-11 18:35:59 +0900

    feat: override Mui list theme

---
+++ src/admin/components/utilities/Theme/overrides/ListItemButton.tsx
@@ -0,0 +1,13 @@
+const ListItemButton = () => {
+  return {
+    MuiListItemButton: {
+      defaultProps: {
+        sx: {
+          p: '4px 0 4px 24px',
+        },
+      },
+    },
+  };
+};
+
+export default ListItemButton;
