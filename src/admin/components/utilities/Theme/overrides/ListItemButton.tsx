c64ba4755a38675d81aa853ce1a90ff98db8bb22
kazaneshimizu <ks@rocketa.co.jp>
2022-12-11 23:36:38 +0900

    style: add border on right side

--- src/admin/components/utilities/Theme/overrides/ListItemButton.tsx
+++ src/admin/components/utilities/Theme/overrides/ListItemButton.tsx
@@ -1,9 +1,18 @@
-const ListItemButton = () => {
+import { Theme } from '@mui/material';
+
+const ListItemButton = (theme: Theme) => {
   return {
     MuiListItemButton: {
       defaultProps: {
         sx: {
           p: '4px 0 4px 24px',
+        },
+      },
+      styleOverrides: {
+        root: {
+          '&.Mui-selected': {
+            borderRight: `2px solid ${theme.palette.primary.main}`,
+          },
         },
       },
     },
