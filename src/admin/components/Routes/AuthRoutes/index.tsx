6beaf44b08a056fbbbc694aa95e4287e1bb38459
kazaneshimizu <ks@rocketa.co.jp>
2022-12-12 23:03:02 +0900

    refactor: change package path

--- src/admin/components/Routes/AuthRoutes/index.tsx
+++ src/admin/components/Routes/AuthRoutes/index.tsx
@@ -2,10 +2,10 @@ import Loader from '@admin/components/elements/Loader';
 import MinimalLayout from '@admin/components/layouts/Minimal';
 import React, { lazy } from 'react';
 
-const CreateFirstUser = Loader(lazy(() => import('@admin/components/views/CreateFirstUser')));
-const Login = Loader(lazy(() => import('@admin/components/views/Login')));
+const CreateFirstUser = Loader(lazy(() => import('@admin/pages/CreateFirstUser')));
+const Login = Loader(lazy(() => import('@admin/pages/Login')));
 const Logout = Loader(lazy(() => import('@admin/pages/Logout')));
-const Forgot = Loader(lazy(() => import('@admin/components/views/Forgot')));
+const Forgot = Loader(lazy(() => import('@admin/pages/Forgot')));
 
 const AuthRoutes = {
   path: '/admin/auth',
