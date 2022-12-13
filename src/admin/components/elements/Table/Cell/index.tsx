faad2a2bc12d4bb68c485af8e5cfae95d20b81a3
kazaneshimizu <ks@rocketa.co.jp>
2022-12-14 08:48:21 +0900

    feat: add table cell element

---
+++ src/admin/components/elements/Table/Cell/index.tsx
@@ -0,0 +1,36 @@
+import { format } from 'date-fns';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Props, Type } from './types';
+
+const Cell: React.FC<Props> = (props) => {
+  const { colIndex, type, rowData, cellData } = props;
+
+  let WrapElement: React.ComponentType<any> | string = 'span';
+
+  const wrapElementProps: {
+    to?: string;
+  } = {};
+
+  if (colIndex === 0) {
+    WrapElement = Link;
+    wrapElementProps.to = `${rowData.id}`;
+  }
+
+  const sanitizedCellData = () => {
+    switch (type) {
+      case Type.Text || Type.Number:
+        return cellData;
+      case Type.Date:
+        return format(new Date(Number(cellData)), 'yyyy-MM-dd HH:mm');
+      case Type.Object:
+        return JSON.stringify(cellData);
+      default:
+        return '';
+    }
+  };
+
+  return <WrapElement {...wrapElementProps}>{sanitizedCellData()}</WrapElement>;
+};
+
+export default Cell;
