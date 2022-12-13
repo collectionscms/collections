5fc46abd1afb4339f5ebf5bf8006941ac0bceb9a
kazaneshimizu <ks@rocketa.co.jp>
2022-12-14 08:55:32 +0900

    feat: add Role page

---
+++ src/admin/pages/Role/index.tsx
@@ -0,0 +1,30 @@
+import Tables from '@admin/components/elements/Table';
+import { Box } from '@mui/material';
+import React from 'react';
+import buildColumns from './buildColumns';
+
+const Role: React.FC = () => {
+  const columns = buildColumns();
+  const rows = [
+    {
+      id: 1,
+      name: 'Admin',
+      description: 'Super Admins can access and manage all features and settings.',
+      createdAt: '1670637496808',
+    },
+    {
+      id: 2,
+      name: 'Editor',
+      description: 'Editors can manage and publish contents including those of other users.',
+      createdAt: '1670648096808',
+    },
+  ];
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Tables columns={columns} rows={rows} />
+    </Box>
+  );
+};
+
+export default Role;
