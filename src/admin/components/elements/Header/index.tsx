8792e59fbe83968cc2b7e9b67788d9cbd9f46a64
kazaneshimizu <ks@rocketa.co.jp>
2022-12-18 09:02:18 +0900

    style: profile area moved to nav

--- src/admin/components/elements/Header/index.tsx
+++ src/admin/components/elements/Header/index.tsx
@@ -1,13 +1,13 @@
-import Profile from '@admin/components/poppers/Profile';
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import { AppBar, IconButton, Toolbar, useMediaQuery, useTheme } from '@mui/material';
+import { AppBar, Box, IconButton, Toolbar, useMediaQuery, useTheme } from '@mui/material';
 import config from '@shared/features/config';
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Props } from './types';
 
 const Header: React.FC<Props> = ({ toggleDrawer }) => {
   const theme = useTheme();
+  const location = useLocation();
   const lgUp = useMediaQuery(theme.breakpoints.up('lg'));
 
   const mainHeader = (
@@ -23,15 +23,13 @@ const Header: React.FC<Props> = ({ toggleDrawer }) => {
       >
         <MenuIcon fontSize="small" />
       </IconButton>
-      <Profile />
     </Toolbar>
   );
 
   return lgUp ? (
     <AppBar color="inherit" sx={{ pl: `${config?.ui.navWidth}px` }}>
       <Toolbar>
-        <SearchIcon fontSize="small" />
-        <Profile />
+        <Box component="header">{location.pathname}</Box>
       </Toolbar>
     </AppBar>
   ) : (
