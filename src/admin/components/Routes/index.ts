264daab725ae079c95c13053e931dabec4a476cb
kazaneshimizu <ks@rocketa.co.jp>
2022-12-18 14:34:10 +0900

    feat: separate routes for collection from main

--- src/admin/components/Routes/index.ts
+++ src/admin/components/Routes/index.ts
@@ -1,7 +1,8 @@
 import { useRoutes } from 'react-router-dom';
 import AuthRoutes from './Auth';
+import CollectionRoutes from './Collection';
 import MainRoutes from './Main';
 
 export default function Routes() {
-  return useRoutes([MainRoutes, AuthRoutes]);
+  return useRoutes([MainRoutes, CollectionRoutes, AuthRoutes]);
 }
