65c506b908d2a6157c8a49d5afb2f5deeb8302fb
kazaneshimizu <ks@rocketa.co.jp>
2022-12-18 23:47:08 +0900

    refactor: changed to implementation using useParams

--- src/admin/components/Routes/Collection/index.tsx
+++ src/admin/components/Routes/Collection/index.tsx
@@ -2,25 +2,12 @@ import Loader from '@admin/components/elements/Loader';
 import CollectionLayout from '@admin/components/layouts/Collection';
 import React, { lazy } from 'react';
 
-const List = lazy(() => import('@admin/pages/collections/List'));
-
-const buildWrapElement = (props: { collection: string }) => {
-  const WrapElement = Loader(List, props);
-  return <WrapElement />;
-};
-
-// TODO Retrieve from DB
-const collections = [{ collection: 'Restaurant' }, { collection: 'Menu' }];
+const List = Loader(lazy(() => import('@admin/pages/collections/List')));
 
 const AppRoutes = {
   path: '/admin/collections',
   element: <CollectionLayout />,
-  children: [
-    ...collections.map((collection) => ({
-      path: `${collection.collection}`,
-      element: <>{buildWrapElement({ ...collection })}</>,
-    })),
-  ],
+  children: [{ path: ':collection', element: <List /> }],
 };
 
 export default AppRoutes;
--- src/admin/pages/collections/List/index.tsx
+++ src/admin/pages/collections/List/index.tsx
@@ -1,8 +1,47 @@
+import RouterLink from '@admin/components/elements/Link';
+import { Box, BoxProps, Button } from '@mui/material';
 import React from 'react';
-import { Props } from './types';
+import { useParams } from 'react-router-dom';
 
-const List: React.FC<Props> = ({ collection }) => {
-  return <p>{collection}</p>;
+const Item = (props: BoxProps) => {
+  const { sx, ...other } = props;
+  return (
+    <Box
+      sx={{
+        alignItems: 'center',
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+};
+
+const List: React.FC = () => {
+  const { collection } = useParams();
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+      }}
+    >
+      <Item>
+        <h1>{collection}</h1>
+      </Item>
+      <Item>
+        <Button
+          variant="contained"
+          component={RouterLink}
+          to={`/admin/collections/${collection}/create`}
+        >
+          登録
+        </Button>
+      </Item>
+    </Box>
+  );
 };
 
 export default List;
