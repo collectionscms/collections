7f8e7e407d8d7659fa89e754e115c75fa9dad079
kazaneshimizu <ks@rocketa.co.jp>
2022-12-18 16:39:20 +0900

    refactor: change property type to collection

--- src/admin/components/Routes/Collection/index.tsx
+++ src/admin/components/Routes/Collection/index.tsx
@@ -4,20 +4,20 @@ import React, { lazy } from 'react';
 
 const List = lazy(() => import('@admin/pages/collections/List'));
 
-const buildWrapElement = (props: { type: string }) => {
+const buildWrapElement = (props: { collection: string }) => {
   const WrapElement = Loader(List, props);
   return <WrapElement />;
 };
 
 // TODO Retrieve from DB
-const collections = [{ type: 'Restaurant' }, { type: 'Menu' }];
+const collections = [{ collection: 'Restaurant' }, { collection: 'Menu' }];
 
 const AppRoutes = {
   path: '/admin/collections',
   element: <CollectionLayout />,
   children: [
     ...collections.map((collection) => ({
-      path: `${collection.type}`,
+      path: `${collection.collection}`,
       element: <>{buildWrapElement({ ...collection })}</>,
     })),
   ],
