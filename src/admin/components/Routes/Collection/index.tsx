d336468fde455c7119bd63c6e33a77a5b88181f3
kazaneshimizu <ks@rocketa.co.jp>
2022-12-19 07:58:49 +0900

    chore: add route to create, edit

--- src/admin/components/Routes/Collection/index.tsx
+++ src/admin/components/Routes/Collection/index.tsx
@@ -3,11 +3,17 @@ import CollectionLayout from '@admin/components/layouts/Collection';
 import React, { lazy } from 'react';
 
 const List = Loader(lazy(() => import('@admin/pages/collections/List')));
+const Create = Loader(lazy(() => import('@admin/pages/collections/Create')));
+const Edit = Loader(lazy(() => import('@admin/pages/collections/Edit')));
 
-const AppRoutes = {
+const CollectionRoutes = {
   path: '/admin/collections',
   element: <CollectionLayout />,
-  children: [{ path: ':collection', element: <List /> }],
+  children: [
+    { path: ':collection', element: <List /> },
+    { path: ':collection/create', element: <Create /> },
+    { path: ':collection/:id', element: <Edit /> },
+  ],
 };
 
-export default AppRoutes;
+export default CollectionRoutes;
