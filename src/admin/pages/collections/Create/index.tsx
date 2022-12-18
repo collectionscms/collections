3323c19422137cd8c57d091b4eae31e8ae893c61
kazaneshimizu <ks@rocketa.co.jp>
2022-12-19 07:57:56 +0900

    feat: create page

---
+++ src/admin/pages/collections/Create/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Create: React.FC = () => {
+  return <p>Create</p>;
+};
+
+export default Create;
---
+++ src/admin/pages/collections/Edit/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Edit: React.FC = () => {
+  return <p>Edit</p>;
+};
+
+export default Edit;
