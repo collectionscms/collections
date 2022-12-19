be73a61615a93d74cbc1de80b50a860b991ad451
kazaneshimizu <ks@rocketa.co.jp>
2022-12-19 22:47:17 +0900

    chore: rendering of input form

--- src/admin/pages/collections/List/Singleton.tsx
+++ src/admin/pages/collections/List/Singleton.tsx
@@ -1,5 +1,7 @@
 import RouterLink from '@admin/components/elements/Link';
-import { Box, BoxProps, Button } from '@mui/material';
+import { Type } from '@admin/components/elements/Table/Cell/types';
+import { Box, BoxProps, Button, TextField } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 import React from 'react';
 import { Props } from './types';
 
@@ -17,23 +19,47 @@ const Item = (props: BoxProps) => {
 };
 
 const Singleton: React.FC<Props> = ({ collection }) => {
+  // TODO Retrieve from DB
+  const fields = [
+    { accessor: 'name', type: Type.Text },
+    { accessor: 'address', type: Type.Text },
+  ];
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        width: '100%',
-      }}
-    >
-      <Item>
-        <h1>{collection}</h1>
-      </Item>
-      <Item>
-        <Button variant="contained" component={RouterLink} to={`/admin/collections/${collection}`}>
-          更新
-        </Button>
-      </Item>
+    <Box sx={{ width: '100%' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <Item>
+          <h1>{collection}</h1>
+        </Item>
+        <Item>
+          <Button
+            variant="contained"
+            component={RouterLink}
+            to={`/admin/collections/${collection}`}
+          >
+            更新
+          </Button>
+        </Item>
+      </Box>
+      <Grid container spacing={3}>
+        {fields.map((field) => (
+          <Grid xs={12} md={6}>
+            <Item>
+              <Box>
+                <label>{field.accessor}</label>
+              </Box>
+              <TextField id={field.accessor} fullWidth />
+            </Item>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 };
