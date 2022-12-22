f05667cddd61c741432f1c3c23a293cc06019af6
kazaneshimizu <ks@rocketa.co.jp>
2022-12-23 08:02:08 +0900

    feat: Replaced Mui icon with fontawesome.

--- src/admin/components/elements/NavItem/index.tsx
+++ src/admin/components/elements/NavItem/index.tsx
@@ -1,15 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import React from 'react';
 import RouterLink from '../Link';
 import { Props } from './types';
 
 const NavItem: React.FC<Props> = ({ item }) => {
-  const { href, label, Icon } = item;
+  const { href, label, icon } = item;
 
   return (
     <ListItemButton to={href} component={RouterLink}>
       <ListItemIcon>
-        <Icon />
+        <FontAwesomeIcon icon={icon} />
       </ListItemIcon>
       <ListItemText primary={label} />
     </ListItemButton>
--- src/admin/components/elements/ToggleColor/index.tsx
+++ src/admin/components/elements/ToggleColor/index.tsx
@@ -1,26 +1,25 @@
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Box } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
-import { useTheme } from '@mui/material/styles';
 import React, { useContext } from 'react';
 import ColorModeContext from './context';
 
 const ToggleColor: React.FC = () => {
-  const theme = useTheme();
   const colorMode = useContext(ColorModeContext);
 
   return (
     <Box
       sx={{
-        width: '100%',
-        alignItems: 'left',
-        fontSize: 20,
-        p: 3,
+        width: '60px',
+        height: '60px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <IconButton onClick={colorMode.toggleColorMode} color="inherit" sx={{ p: 0 }}>
-        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+        <FontAwesomeIcon icon={faCircleHalfStroke} />
       </IconButton>
     </Box>
   );
