5691d15e18b5271783be6cf6ed7537977b8cbdb2
kazaneshimizu <ks@rocketa.co.jp>
2022-12-24 13:32:07 +0900

    feat: Document information provider added.

---
+++ src/admin/components/utilities/DocumentInfo/index.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useMemo } from 'react';
+import { ContextType, Props } from './types';
+
+const Context = createContext({} as ContextType);
+
+export const DocumentInfoProvider: React.FC<Props> = (props) => {
+  const { label, children } = props;
+
+  const value = useMemo(
+    () => ({
+      label,
+    }),
+    [props]
+  );
+
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
+
+export const useDocumentInfo = (): ContextType => useContext(Context);
+
+export default Context;
