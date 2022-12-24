6bb77b21526e202fc86da4d1b089da95fa2c5f81
kazaneshimizu <ks@rocketa.co.jp>
2022-12-24 13:38:07 +0900

    refactor: Passing the label to the page via provider.

--- src/admin/components/Routes/Setting/index.tsx
+++ src/admin/components/Routes/Setting/index.tsx
@@ -1,30 +1,45 @@
 import Loader from '@admin/components/elements/Loader';
 import MainLayout from '@admin/components/layouts/Main';
-import ContentType from '@admin/pages/ContentType';
+import { DocumentInfoProvider } from '@admin/components/utilities/DocumentInfo';
 import { settingsGroupNavItems } from '@admin/utilities/groupNavItems';
 import React, { lazy } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const User = Loader(lazy(() => import('@admin/pages/User')));
 const Role = Loader(lazy(() => import('@admin/pages/Role')));
-const groups = settingsGroupNavItems();
+const ContentType = Loader(lazy(() => import('@admin/pages/ContentType')));
+const group = settingsGroupNavItems();
 
 const SettingRoutes = {
   path: '/admin/settings',
-  element: <MainLayout groups={groups} />,
+  element: <MainLayout group={group} />,
   children: [
-    { path: '', element: <Navigate to={groups[0].items[0].href} replace /> },
+    { path: '', element: <Navigate to={group.items[0].href} replace /> },
     {
       path: 'content-types',
-      element: <ContentType />,
+      element: (
+        <DocumentInfoProvider
+          label={group.items.find((group) => group.id == 'content-types').label}
+        >
+          <ContentType />
+        </DocumentInfoProvider>
+      ),
     },
     {
       path: 'users',
-      element: <User />,
+      element: (
+        <DocumentInfoProvider label={group.items.find((group) => group.id == 'users').label}>
+          <User />
+        </DocumentInfoProvider>
+      ),
     },
     {
       path: 'roles',
-      element: <Role />,
+      element: (
+        <DocumentInfoProvider label={group.items.find((group) => group.id == 'roles').label}>
+          <Role />
+        </DocumentInfoProvider>
+      ),
     },
   ],
 };
