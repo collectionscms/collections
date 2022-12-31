52a083f19fc1164b2b638e3680ecbeaabf0a2cf0
kazaneshimizu <ks@rocketa.co.jp>
2023-01-01 06:39:47 +0900

    style: Add theme.

---
+++ src/admin/components/utilities/Theme/Default/typography.ts
@@ -0,0 +1,8 @@
+import { TypographyOptions } from '@mui/material/styles/createTypography';
+
+const typography: TypographyOptions = {
+  htmlFontSize: 16,
+  fontFamily: ['Roboto', 'Noto Sans JP', 'Helvetica', 'Arial', 'Meiryo', 'sans-serif'].join(','),
+};
+
+export default typography;
