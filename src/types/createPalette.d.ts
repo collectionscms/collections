f1d135c39ade9c8d9ce8a1e19ed586a4c58e8dc8
kazaneshimizu <ks@rocketa.co.jp>
2023-01-01 06:41:08 +0900

    style: Change the theme to match mode.

---
+++ src/types/createPalette.d.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/prefer-default-export
+export declare module '@mui/material/styles' {
+  interface Palette {
+    sidebar: Palette['primary'];
+  }
+
+  interface PaletteOptions {
+    sidebar?: PaletteOptions['primary'];
+  }
+}
