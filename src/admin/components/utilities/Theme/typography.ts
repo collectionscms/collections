80d71ad52d33e536229d3b42e9a0b339b044bc7c
kazaneshimizu <ks@rocketa.co.jp>
2023-01-01 06:42:32 +0900

    chore: Delete as not used.

--- src/admin/components/utilities/Theme/typography.ts
+++
@@ -1,8 +0,0 @@
-import { TypographyOptions } from '@mui/material/styles/createTypography';
-
-const Typography = (fontFamily: string): TypographyOptions => ({
-  htmlFontSize: 16,
-  fontFamily,
-});
-
-export default Typography;
