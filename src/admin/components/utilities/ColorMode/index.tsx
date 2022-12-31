1f2b20da3117a541a966185426c5b34a8f9044c7
kazaneshimizu <ks@rocketa.co.jp>
2022-12-31 11:49:06 +0900

    chore: Add Color mode provider.

---
+++ src/admin/components/utilities/ColorMode/index.tsx
@@ -0,0 +1,75 @@
+import React, { createContext, useCallback, useContext, useMemo, useState } from 'react';
+
+export type Mode = 'light' | 'dark';
+
+export type ModeContext = {
+  mode: Mode;
+  setMode: (mode: Mode) => void;
+  autoMode: boolean;
+};
+
+const localStorageKey = 'superfast-color-mode';
+
+const initialContext: ModeContext = {
+  mode: 'light',
+  setMode: () => null,
+  autoMode: true,
+};
+
+const Context = createContext(initialContext);
+
+const getMode = () => {
+  let mode: Mode;
+  const modeFromStorage = window.localStorage.getItem(localStorageKey);
+
+  if (modeFromStorage === 'light' || modeFromStorage === 'dark') {
+    mode = modeFromStorage;
+  } else {
+    mode =
+      window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light';
+  }
+
+  return mode;
+};
+
+export const ColorModeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [mode, setModeState] = useState<Mode>(getMode);
+  const [autoMode, setAutoMode] = useState(() => {
+    const modeFromStorage = window.localStorage.getItem(localStorageKey);
+    return !modeFromStorage;
+  });
+
+  const setMode = useCallback((modeToSet: Mode | 'auto') => {
+    if (modeToSet === 'light' || modeToSet === 'dark') {
+      setAutoMode(false);
+      setModeState(modeToSet);
+      window.localStorage.setItem(localStorageKey, modeToSet);
+    } else if (modeToSet === 'auto') {
+      const existingModeFromStorage = window.localStorage.getItem(localStorageKey);
+      if (existingModeFromStorage) window.localStorage.removeItem(localStorageKey);
+      const modeFromOS =
+        window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+          ? 'dark'
+          : 'light';
+      setAutoMode(true);
+      setModeState(modeFromOS);
+    }
+  }, []);
+
+  const value = useMemo(
+    () => ({
+      mode,
+      setMode,
+      autoMode,
+    }),
+    [mode, autoMode]
+  );
+
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
+
+export const useColorMode = (): ModeContext => useContext(Context);
+
+export default Context;
