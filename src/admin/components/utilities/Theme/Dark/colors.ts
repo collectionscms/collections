80f6180b7e091f335eaa81453614d8526c5229a3
kazaneshimizu <ks@rocketa.co.jp>
2023-01-01 06:39:07 +0900

    style: Add Dark theme.

---
+++ src/admin/components/utilities/Theme/Dark/colors.ts
@@ -0,0 +1,14 @@
+import { PaletteOptions } from '@mui/material';
+
+const colors: PaletteOptions = {
+  mode: 'dark',
+  primary: { main: '#fff' },
+  secondary: { main: '#eaff96' },
+  background: {
+    default: '#151515',
+    paper: '#151515',
+  },
+  sidebar: { main: '#222' },
+};
+
+export default colors;
---
+++ src/admin/components/utilities/Theme/Dark/index.ts
@@ -0,0 +1,8 @@
+import { createTheme } from '@mui/material';
+import colors from './colors';
+import typography from './typography';
+
+export default createTheme({
+  palette: colors,
+  typography,
+});
