dddce7bc0c2cae26805d00c17efe3a8db984f4ea
kazaneshimizu <ks@rocketa.co.jp>
2023-01-03 12:15:15 +0900

    feat: Disable the action button if has not permission.

--- src/admin/pages/collections/Edit/index.tsx
+++ src/admin/pages/collections/Edit/index.tsx
@@ -1,13 +1,55 @@
+import RouterLink from '@admin/components/elements/Link';
+import { useAuth } from '@admin/components/utilities/Auth';
+import { BoxProps, Box, Button } from '@mui/material';
+import { t } from 'i18next';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Props } from './types';
 
+const Item = (props: BoxProps) => {
+  const { sx, ...other } = props;
+  return (
+    <Box
+      sx={{
+        alignItems: 'center',
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+};
+
 const EditPage: React.FC<Props> = ({ collection }) => {
   const { id } = useParams();
+  const { hasPermission } = useAuth();
 
   return (
     <>
-      {id ? 'Edit' : 'Create'} {collection.collection}
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+        }}
+      >
+        <Item>
+          <h1>
+            {id ? 'Update' : 'Create'} {collection.collection}
+          </h1>
+        </Item>
+        <Item>
+          <Button
+            variant="contained"
+            disabled={!hasPermission(collection.collection, id ? 'update' : 'create')}
+            component={RouterLink}
+            to="create"
+          >
+            {id ? t('button.update') : t('button.create')}
+          </Button>
+        </Item>
+      </Box>
+      {}
     </>
   );
 };
