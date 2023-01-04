47ffc6e802a0790533afca84a2597dadd180f431
kazaneshimizu <ks@rocketa.co.jp>
2023-01-05 08:41:56 +0900

    refactor: Delete profile setting.

--- src/admin/pages/User/Edit/index.tsx
+++ src/admin/pages/User/Edit/index.tsx
@@ -1,18 +1,5 @@
 import RouterLink from '@admin/components/elements/Link';
-import { useColorMode } from '@admin/components/utilities/ColorMode';
-import { Mode } from '@admin/components/utilities/ColorMode/types';
-import {
-  Button,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-  MenuItem,
-  Radio,
-  RadioGroup,
-  Select,
-  SelectChangeEvent,
-  Stack,
-} from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -22,12 +9,7 @@ export type OnChange<T = string> = (value: T) => void;
 
 const EditPage: React.FC = () => {
   const { id } = useParams();
-  const { t, i18n } = useTranslation();
-  const { mode, setMode, autoMode } = useColorMode();
-
-  const handleChange = (event: SelectChangeEvent) => {
-    i18n.changeLanguage(event.target.value);
-  };
+  const { t } = useTranslation();
 
   return (
     <Stack rowGap={3}>
@@ -41,37 +23,6 @@ const EditPage: React.FC = () => {
               {id ? t('button.update') : t('button.create')}
             </Button>
           </Grid>
-        </Grid>
-      </Grid>
-      <Grid container spacing={3} xs={12} xl={6}>
-        <Grid xs={12} md={6}>
-          <FormControl fullWidth>
-            <FormLabel>{t('label.language')}</FormLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              displayEmpty
-              value={i18n.language}
-              onChange={handleChange}
-            >
-              <MenuItem value="ja">{t('label.japanese')}</MenuItem>
-              <MenuItem value="en">English</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid xs={12} md={6}>
-          <FormControl>
-            <FormLabel>{t('label.theme')}</FormLabel>
-            <RadioGroup
-              row
-              value={autoMode ? 'auto' : mode}
-              onChange={(e) => setMode(e.target.value as Mode)}
-            >
-              <FormControlLabel value="auto" control={<Radio />} label={t('label.automatic')} />
-              <FormControlLabel value="light" control={<Radio />} label={t('label.light')} />
-              <FormControlLabel value="dark" control={<Radio />} label={t('label.dark')} />
-            </RadioGroup>
-          </FormControl>
         </Grid>
       </Grid>
     </Stack>
