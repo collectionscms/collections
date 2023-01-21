9fc6a769ade2f58c85b6d159e3fed302c118a051
kazaneshimizu <ks@rocketa.co.jp>
2023-01-21 10:03:17 +0900

    refactor: Change to bin.ts.

---
+++ src/bin.ts
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import packageJSON from '../package.json';
+
+const program = new Command();
+
+const start = async () => {
+  process.env.NODE_ENV = process.env.NODE_ENV ?? 'production';
+  await require('@scripts/start').default();
+};
+
+const build = async () => {
+  process.env.NODE_ENV = process.env.NODE_ENV ?? 'production';
+  await require('@scripts/build').default();
+};
+
+const dev = async () => {
+  process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
+  await require('@scripts/dev').default();
+};
+
+// Initial program setup
+program.storeOptionsAsProperties(false).allowUnknownOption(true);
+
+program.helpOption('-h, --help', 'Display help for command');
+program.addHelpCommand('help [command]', 'Display help for command');
+
+// `$ superfast version` (--version synonym)
+program.version(packageJSON.version, '-v, --version', 'Output the version number');
+
+program
+  .command('version')
+  .description('Output your version of Superfast')
+  .action(() => {
+    process.stdout.write(`${packageJSON.version}\n`);
+    process.exit(0);
+  });
+
+program.command('start').description('Start your Superfast application').action(start);
+program.command('build').description('Build your Superfast application').action(build);
+program.command('dev').description('Develop your Superfast server').action(dev);
+
+program.parseAsync(process.argv);
