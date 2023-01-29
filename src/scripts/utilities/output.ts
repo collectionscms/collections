79cfd3dae4c8996550eec6f12a66c0ff89a150cc
kazaneshimizu <ks@rocketa.co.jp>
2023-01-29 23:13:42 +0900

    chore: Change prefix icon.

--- src/scripts/utilities/output.ts
+++ src/scripts/utilities/output.ts
@@ -1,9 +1,7 @@
-import chalk from 'chalk';
-
 const Output = {
-  success: (...output: any[]) => console.log(`✅ `, ...output),
+  info: (...output: any[]) => console.log(`✨ `, ...output),
+  success: (...output: any[]) => console.log(`⭐️ `, ...output),
   error: (...output: any[]) => console.log(`❌ `, ...output),
-  info: (...output: any[]) => console.log(chalk.blue(...output)),
 };
 
 export default Output;
