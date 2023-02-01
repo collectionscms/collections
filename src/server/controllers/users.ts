80994fd96f60c98a06837dc9e8790f735b70628c
kazaneshimizu <ks@rocketa.co.jp>
2023-02-02 07:35:09 +0900

    refactor: Core module removed, user list API added.

--- src/server/controllers/users.ts
+++ src/server/controllers/users.ts
@@ -1,9 +1,34 @@
 import asyncMiddleware from '../middleware/async';
-import express from 'express';
-import { users } from 'superfast-core';
+import { PrismaClient } from '@prisma/client';
+import express, { Request, Response } from 'express';
 
+const prisma = new PrismaClient();
 const app = express();
 
-app.get('/users', asyncMiddleware(users));
+app.get(
+  '/users',
+  asyncMiddleware(async (req: Request, res: Response) => {
+    const users = await prisma.superfast_User.findMany({ include: { superfast_Role: true } });
+
+    res.json({
+      users: users.flatMap((user) => ({
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userName: user.userName,
+        email: user.email,
+        isActive: user.isActive,
+        role: {
+          id: user.superfast_Role.id,
+          name: user.superfast_Role.name,
+          description: user.superfast_Role.description,
+          adminAccess: user.superfast_Role.adminAccess,
+        },
+        createdAt: user.createdAt.getTime(),
+        updatedAt: user.updatedAt.getTime(),
+      })),
+    });
+  })
+);
 
 export default app;
