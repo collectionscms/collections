865b1566196a3bfed103e1d83cd65a672bf189a7
kazaneshimizu <ks@rocketa.co.jp>
2023-02-12 08:28:52 +0900

    refactor: Remove prisma.

--- src/scripts/utilities/copyCommonFiles.ts
+++ src/scripts/utilities/copyCommonFiles.ts
@@ -5,19 +5,8 @@ import path from 'path';
 
 export const copyCommonFiles = async (projectDir: string, projectName: string): Promise<void> => {
   const templateDir = path.join(__dirname, '../../', 'templates', 'default');
-  const prismaDir = path.join(__dirname, '../../../', 'prisma');
 
   Output.info(`Copy files to directory ${chalk.magentaBright(projectName)}.`);
-
-  // schema.prisma
-  await fse.copy(prismaDir, `${projectDir}/prisma`, {
-    filter: (dest) => {
-      if (dest.indexOf('migrations') == -1 && fse.lstatSync(dest).isDirectory()) {
-        return true;
-      }
-      return dest.indexOf('schema.prisma') > -1;
-    },
-  });
 
   // template
   await fse.copy(templateDir, projectDir);
