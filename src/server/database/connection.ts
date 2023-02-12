ec0e23ed7ff5241bb1dc06e55d174dd3a6be5176
kazaneshimizu <ks@rocketa.co.jp>
2023-02-12 09:41:11 +0900

    refactor: Set DB connection information written in environment variables.

--- src/server/database/connection.ts
+++ src/server/database/connection.ts
@@ -1,5 +1,6 @@
 import knex, { Knex } from 'knex';
 import path from 'path';
+import 'dotenv/config';
 
 let database: Knex | null = null;
 
@@ -8,13 +9,14 @@ export const getDatabase = async (): Promise<Knex> => {
   let migrationFiles = path.join(__dirname, 'migrations');
 
   const config: Knex.Config = {
-    client: 'sqlite3',
+    client: process.env.DB_CLIENT,
     connection: {
-      filename: './data.db',
+      filename: process.env.DB_FILENAME,
     },
     useNullAsDefault: true,
     migrations: {
       directory: migrationFiles,
+      loadExtensions: [process.env.MIGRATE_EXTENSIONS],
     },
     postProcessResponse: (result) => {
       if (Array.isArray(result)) {
