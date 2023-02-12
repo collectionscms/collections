6d50bcb109ea581f588b458cf2c4f5d303fb0fb1
kazaneshimizu <ks@rocketa.co.jp>
2023-02-12 09:42:39 +0900

    feat: Add DB environment variables.

--- src/scripts/utilities/writeEnvFile.ts
+++ src/scripts/utilities/writeEnvFile.ts
@@ -3,6 +3,6 @@ import fse from 'fs-extra';
 
 export const writeEnvFile = async (projectDir: string, databaseName: string): Promise<void> => {
   Output.info('Create .env file.');
-  const content = `DATABASE_URL="file:./${databaseName}.db"`;
+  const content = `DB_CLIENT="sqlite3"\nDB_FILENAME="./${databaseName}.db"\nMIGRATE_EXTENSIONS=".js"`;
   await fse.outputFile(`${projectDir}/.env`, content);
 };
