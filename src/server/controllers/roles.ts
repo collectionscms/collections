26daf1a65d54e3d2e4c112d5b0d01de9119d3993
kazaneshimizu <ks@rocketa.co.jp>
2023-02-21 22:24:12 +0900

    feat: Added API to get permissions.

--- src/server/controllers/roles.ts
+++ src/server/controllers/roles.ts
@@ -1,3 +1,4 @@
+import { Permission, Role } from '@shared/types';
 import express, { Request, Response } from 'express';
 import { getDatabase } from '../database/connection';
 import asyncMiddleware from '../middleware/async';
@@ -8,8 +9,22 @@ app.get(
   '/roles',
   asyncMiddleware(async (req: Request, res: Response) => {
     const database = await getDatabase();
-    const roles = await database('superfast_roles');
+    const roles = await database<Role>('superfast_roles');
     res.json({ roles: roles });
+  })
+);
+
+app.get(
+  '/roles/:id/permissions',
+  asyncMiddleware(async (req: Request, res: Response) => {
+    const database = await getDatabase();
+    const id = req.params.id;
+
+    const permissions = await database<Permission>('superfast_permissions').where(
+      'superfast_role_id',
+      id
+    );
+    res.json({ permissions: permissions });
   })
 );
 
