0e053e69216845c4b2b832839978bae656c6c280
kazaneshimizu <ks@rocketa.co.jp>
2023-02-26 07:47:35 +0900

    feat: Add admin access field to role registration.

--- src/admin/fields/schemas/roles/createRole.ts
+++ src/admin/fields/schemas/roles/createRole.ts
@@ -4,12 +4,14 @@ import yup from '../../yup';
 export type FormValues = {
   name: string;
   description: string;
+  adminAccess: boolean;
 };
 
 export const createRole = (): ObjectSchema<FormValues> => {
   return yup.object().shape({
     name: yup.string().required().max(60),
     description: yup.string().max(250),
+    adminAccess: yup.boolean(),
   });
 };
 
