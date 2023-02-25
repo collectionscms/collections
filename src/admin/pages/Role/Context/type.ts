d73eeaa8010ee9bb59625fdecffa6b9c27beca97
kazaneshimizu <ks@rocketa.co.jp>
2023-02-26 07:10:01 +0900

    feat: Implement role registration.

--- src/admin/pages/Role/Context/type.ts
+++ src/admin/pages/Role/Context/type.ts
@@ -1,6 +1,8 @@
-import { Role } from '@shared/types';
 import { SWRResponse } from 'swr';
+import { SWRMutationResponse } from 'swr/mutation';
+import { Role } from '../../../../shared/types';
 
 export type RoleContext = {
   getRoles: () => SWRResponse<Role[]>;
+  createRole: () => SWRMutationResponse<Role>;
 };
