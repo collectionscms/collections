3bb6a84fd3027b100ceba9bc4c9997de09922559
kazaneshimizu <ks@rocketa.co.jp>
2023-02-27 18:37:43 +0900

    feat: Logo & project name displayed on login page.

---
+++ src/admin/pages/Login/Context/index.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext } from 'react';
+import useSWR, { SWRConfiguration } from 'swr';
+import { ProjectSetting } from '../../../../shared/types';
+import api from '../../../utilities/api';
+import { LoginContext } from './type';
+
+const Context = createContext({} as LoginContext);
+
+export const LoginContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const getProjectSetting = (config?: SWRConfiguration) =>
+    useSWR(
+      '/project_settings',
+      (url) =>
+        api
+          .get<{ projectSetting: ProjectSetting }>(url)
+          .then((res) => res.data.projectSetting)
+          .catch((err) => Promise.reject(err.message)),
+      config
+    );
+
+  return (
+    <Context.Provider
+      value={{
+        getProjectSetting,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useLogin = () => useContext(Context);
