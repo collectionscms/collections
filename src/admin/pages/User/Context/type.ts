5719f49bdf2c30bc06d886f0940e15120b074917
kazaneshimizu <ks@rocketa.co.jp>
2023-03-02 22:56:36 +0900

    refactor: Defined SWRMutationResponse response type for create user.

--- src/admin/pages/User/Context/type.ts
+++ src/admin/pages/User/Context/type.ts
@@ -1,11 +1,11 @@
-import { Role, User } from '@shared/types';
 import { SWRConfiguration, SWRResponse } from 'swr';
 import { SWRMutationResponse } from 'swr/mutation';
+import { Role, User } from '../../../../shared/types';
 
 export type UserContext = {
   getUsers: () => SWRResponse<User[]>;
   getUser: (id: string, config?: SWRConfiguration) => SWRResponse<User>;
   getRoles: (config?: SWRConfiguration) => SWRResponse<Role[]>;
-  createUser: () => SWRMutationResponse<User>;
+  createUser: () => SWRMutationResponse<User, any, Record<string, any>, any>;
   updateUser: (id: string) => SWRMutationResponse;
 };
