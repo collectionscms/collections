4859b44f510ce89dbdfac6e77043bd995fc8e171
kazaneshimizu <ks@rocketa.co.jp>
2023-03-02 22:33:47 +0900

    refactor: Defined SWRMutationResponse response type for create field.

--- src/admin/pages/ContentType/Edit/CreateField/Context/type.ts
+++ src/admin/pages/ContentType/Edit/CreateField/Context/type.ts
@@ -1,6 +1,6 @@
-import { Field } from '@shared/types';
 import { SWRMutationResponse } from 'swr/mutation';
+import { Field } from '../../../../../../shared/types';
 
 export type FieldContext = {
-  createField: (slug: string) => SWRMutationResponse<Field>;
+  createField: (slug: string) => SWRMutationResponse<Field, any, Record<string, any>, any>;
 };
