ebb1283993ff096a5e91f9a2914c5aa01043eb1c
kazaneshimizu <ks@rocketa.co.jp>
2023-03-10 09:14:59 +0900

    chore: Added factory method for testing.

---
+++ test/utilities/factories.ts
@@ -0,0 +1,9 @@
+export const randomString = (length: number): string => {
+  const randomChars =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789あいうえおアイウエオｱｲｳｴｵ０１２３４５６７８９';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+  }
+  return result;
+};
