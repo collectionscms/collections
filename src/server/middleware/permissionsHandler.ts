2f04eb24cf5a70548c1c71584d64d79769385cf7
kazaneshimizu <ks@rocketa.co.jp>
2023-03-11 22:21:18 +0900

    refactor: Added  authentication and collection permissions checking middleware.

--- src/server/middleware/permissionsHandler.ts
+++ src/server/middleware/permissionsHandler.ts
@@ -4,6 +4,32 @@ import { InvalidCredentialsException } from '../../shared/exceptions/invalidCred
 import { Permission, PermissionsAction } from '../../shared/types';
 import { getDatabase } from '../database/connection';
 
+export const collectionPermissionsHandler =
+  (action: PermissionsAction) => async (req: Request, res: Response, next: NextFunction) => {
+    if (!req.userId) {
+      next(new InvalidCredentialsException('invalid_user_credentials'));
+    }
+
+    if (!req.adminAccess) {
+      const database = await getDatabase();
+      const userPermissions = await database<Permission>('superfast_permissions').where(
+        'superfast_role_id',
+        req.roleId
+      );
+
+      const hasPermission = userPermissions.some(
+        (userPermission) =>
+          userPermission.collection === req.params.slug && userPermission.action === action
+      );
+
+      if (!hasPermission) {
+        return next(new ForbiddenException('forbidden'));
+      }
+    }
+
+    return next();
+  };
+
 const permissionsHandler =
   (permissions: { collection: string; action: PermissionsAction }[] = []) =>
   async (req: Request, res: Response, next: NextFunction) => {
