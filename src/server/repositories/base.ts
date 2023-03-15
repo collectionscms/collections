a9727dae191142a06daa0f6249cf47d34fe64bb4
kazaneshimizu <ks@rocketa.co.jp>
2023-03-15 23:07:46 +0900

    feat: Created repository.

---
+++ src/server/repositories/base.ts
@@ -0,0 +1,50 @@
+import type { Knex } from 'knex';
+import { getDatabase } from '../database/connection';
+
+export type AbstractRepositoryOptions = {
+  knex?: Knex;
+};
+
+type AbstractRepository<T> = {
+  read(data: Partial<T>): Promise<T[]>;
+  readOne(id: number | Partial<T>): Promise<T>;
+  create(item: Omit<T, 'id'>): Promise<T>;
+  update(id: number, item: Partial<T>): Promise<void>;
+  delete(id: number): Promise<void>;
+};
+
+export abstract class BaseRepository<T> implements AbstractRepository<T> {
+  collection: string;
+  knex: Knex;
+
+  constructor(collection: string, options: AbstractRepositoryOptions) {
+    this.collection = collection;
+    this.knex = options.knex || getDatabase();
+    return this;
+  }
+
+  public get queryBuilder(): Knex.QueryBuilder {
+    return this.knex(this.collection);
+  }
+
+  read(data: Partial<T>): Promise<T[]> {
+    return this.queryBuilder.where(data).select();
+  }
+
+  readOne(id: number | Partial<T>): Promise<T> {
+    return this.queryBuilder.where('id', id).first();
+  }
+
+  async create(item: Omit<T, 'id'>): Promise<T> {
+    const [output] = await this.queryBuilder.insert<T>(item).returning('*');
+    return output as Promise<T>;
+  }
+
+  update(id: number, item: Partial<T>): Promise<void> {
+    return this.queryBuilder.where('id', id).update(item);
+  }
+
+  delete(id: number): Promise<void> {
+    return this.queryBuilder.where('id', id).del();
+  }
+}
