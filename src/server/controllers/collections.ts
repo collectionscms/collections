b495296cf8d9f0b11f237a268ba7c0c630c91735
kazaneshimizu <ks@rocketa.co.jp>
2023-03-18 11:01:48 +0900

    refactor: Hid DB processing in repository.

--- src/server/controllers/collections.ts
+++ src/server/controllers/collections.ts
@@ -1,8 +1,7 @@
 import express, { Request, Response } from 'express';
-import { Collection } from '../../shared/types';
-import { getDatabase } from '../database/connection';
 import asyncHandler from '../middleware/asyncHandler';
 import permissionsHandler from '../middleware/permissionsHandler';
+import { CollectionsRepository } from '../repositories/collections';
 
 const app = express();
 
@@ -10,9 +9,10 @@ app.get(
   '/collections/:id',
   permissionsHandler(),
   asyncHandler(async (req: Request, res: Response) => {
-    const database = getDatabase();
     const id = Number(req.params.id);
-    const collection = await database('superfast_collections').where('id', id).first();
+    const repository = new CollectionsRepository();
+
+    const collection = await repository.readOne(id);
 
     res.json({
       collection: {
@@ -27,8 +27,9 @@ app.get(
   '/collections',
   permissionsHandler(),
   asyncHandler(async (req: Request, res: Response) => {
-    const database = getDatabase();
-    const collections = await database('superfast_collections');
+    const repository = new CollectionsRepository();
+
+    const collections = await repository.read();
 
     res.json({ collections: collections });
   })
@@ -38,18 +39,18 @@ app.post(
   '/collections',
   permissionsHandler([{ collection: 'superfast_collections', action: 'create' }]),
   asyncHandler(async (req: Request, res: Response) => {
-    const database = getDatabase();
+    const repository = new CollectionsRepository();
 
-    await database.transaction(async (tx) => {
+    await repository.transaction(async (tx) => {
       try {
-        await tx.schema.createTable(req.body.collection, function (table) {
+        await tx.transaction.schema.createTable(req.body.collection, function (table) {
           table.increments();
           table.timestamps(true, true);
         });
 
-        const collections = await tx('superfast_collections').insert(req.body, '*');
+        const collection = await repository.transacting(tx).create(req.body);
 
-        await tx('superfast_fields').insert({
+        await tx.transaction('superfast_fields').insert({
           collection: req.body.collection,
           field: 'id',
           label: 'id',
@@ -59,10 +60,10 @@ app.post(
           hidden: true,
         });
 
-        await tx.commit();
-        res.json({ collection: collections[0] });
+        await tx.transaction.commit();
+        res.json({ collection: collection });
       } catch (e) {
-        await tx.rollback();
+        await tx.transaction.rollback();
         res.status(500).end();
       }
     });
@@ -73,9 +74,10 @@ app.patch(
   '/collections/:id',
   permissionsHandler([{ collection: 'superfast_collections', action: 'update' }]),
   asyncHandler(async (req: Request, res: Response) => {
-    const database = getDatabase();
     const id = Number(req.params.id);
-    await database('superfast_collections').where('id', id).update(req.body);
+    const repository = new CollectionsRepository();
+
+    await repository.update(id, req.body);
 
     res.status(204).end();
   })
@@ -85,21 +87,25 @@ app.delete(
   '/collections/:id',
   permissionsHandler([{ collection: 'superfast_collections', action: 'delete' }]),
   asyncHandler(async (req: Request, res: Response) => {
-    const database = getDatabase();
     const id = Number(req.params.id);
-    const meta = await database<Collection>('superfast_collections').where('id', id).first();
+    const repository = new CollectionsRepository();
 
-    await database.transaction(async (tx) => {
+    const meta = await repository.readOne(id);
+
+    await repository.transaction(async (tx) => {
       try {
-        await tx.schema.dropTable(meta.collection);
-        await tx('superfast_collections').where('id', id).delete();
-        await tx('superfast_fields').where('collection', meta.collection).delete();
-        await tx('superfast_permissions').where('collection', meta.collection).delete();
-        await tx('superfast_relations').where('many_collection', meta.collection).delete();
-        await tx.commit();
+        await tx.transaction.schema.dropTable(meta.collection);
+        await repository.transacting(tx).delete(id);
+        await tx.transaction('superfast_fields').where('collection', meta.collection).delete();
+        await tx.transaction('superfast_permissions').where('collection', meta.collection).delete();
+        await tx
+          .transaction('superfast_relations')
+          .where('many_collection', meta.collection)
+          .delete();
+        await tx.transaction.commit();
         res.status(204).end();
       } catch (e) {
-        await tx.rollback();
+        await tx.transaction.rollback();
         res.status(500).end();
       }
     });
