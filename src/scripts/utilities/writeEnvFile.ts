ebb3461adade8b7ec67ad4c4ff3337708f90979c
kazaneshimizu <ks@rocketa.co.jp>
2023-03-21 08:44:11 +0900

    chore: Add cors env.

--- src/scripts/utilities/writeEnvFile.ts
+++ src/scripts/utilities/writeEnvFile.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 import fse from 'fs-extra';
 import { v4 as uuidv4 } from 'uuid';
@@ -6,14 +7,14 @@ import Output from './output';
 export const writeEnvFile = async (projectDir: string, databaseName: string): Promise<void> => {
   Output.info('Create .env file.');
   const database = `DB_CLIENT="sqlite3"\nDB_FILENAME="./${databaseName}.db"\nMIGRATE_EXTENSIONS=".js"`;
-  // eslint-disable-next-line max-len
   const argon2 = `HASH_MEMORY_COST=4096\nHASH_HASH_LENGTH=32\nHASH_TIME_COST=3\nHASH_PARALLELISM=1\nHASH_TYPE=2\n# HASH_ASSOCIATED_DATA="foo"`;
   const session = `ACCESS_TOKEN_TTL="8h"\nREFRESH_TOKEN_TTL="7d"`;
   const security = `SECRET="${uuidv4()}"`;
   const log = `LOG_LEVEL="info"`;
+  const cors = `CORS_ENABLED=false\nCORS_ORIGIN=false\nCORS_METHODS="GET,POST,PATCH,DELETE"\nCORS_ALLOWED_HEADERS="Content-Type,Authorization"\nCORS_EXPOSED_HEADERS="Content-Range"\nCORS_CREDENTIALS=true\nCORS_MAX_AGE=18000`;
 
   await fse.outputFile(
     `${projectDir}/.env`,
-    `${database}\n\n${argon2}\n\n${session}\n\n${security}\n\n${log}`
+    `${database}\n\n${argon2}\n\n${session}\n\n${security}\n\n${log}\n\n${cors}`
   );
 };
