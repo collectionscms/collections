89a67c4029bb8ca183b587ba757e982f4b6c2c4e
kazaneshimizu <ks@rocketa.co.jp>
2023-03-30 09:14:43 +0900

    feat: Added Select dropdown field component.

---
+++ src/admin/components/forms/field-types/SelectDropdown/index.tsx
@@ -0,0 +1,38 @@
+import { MenuItem, Select } from '@mui/material';
+import React from 'react';
+import { Controller } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { Props } from '../types';
+
+const SelectDropdown: React.FC<Props> = ({ control, register, field: meta }) => {
+  const { t } = useTranslation();
+  const required = meta.required && { required: t('yup.mixed.required') };
+
+  return (
+    <Controller
+      name={meta.field}
+      control={control}
+      defaultValue={''}
+      render={({ field, fieldState }) => (
+        <Select
+          {...field}
+          error={fieldState.error !== undefined}
+          {...register(meta.field, { ...required })}
+          defaultValue={''}
+          fullWidth
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          {meta.fieldOption.choices.map((choice) => (
+            <MenuItem value={choice.value} key={choice.value}>
+              {choice.label}
+            </MenuItem>
+          ))}
+        </Select>
+      )}
+    />
+  );
+};
+
+export default SelectDropdown;
