57e475af9b139dc6b14d6d297699dabea3e82ae7
kazaneshimizu <ks@rocketa.co.jp>
2023-04-01 10:00:55 +0900

    feat: Added validation of publication status.

--- src/admin/fields/schemas/collections/updateCollection.ts
+++ src/admin/fields/schemas/collections/updateCollection.ts
@@ -4,12 +4,26 @@ import yup from '../../yup';
 export type FormValues = {
   hidden: boolean;
   singleton: boolean;
+  status: boolean;
+  draftLabel: string;
+  draftValue: string;
+  publishLabel: string;
+  publishValue: string;
+  closeLabel: string;
+  closeValue: string;
 };
 
 export const updateCollection = (): ObjectSchema<FormValues> => {
   return yup.object().shape({
     hidden: yup.boolean(),
     singleton: yup.boolean(),
+    status: yup.boolean(),
+    draftLabel: yup.string().required().max(60),
+    draftValue: yup.string().required().max(60),
+    publishLabel: yup.string().required().max(60),
+    publishValue: yup.string().required().max(60),
+    closeLabel: yup.string().required().max(60),
+    closeValue: yup.string().required().max(60),
   });
 };
 
