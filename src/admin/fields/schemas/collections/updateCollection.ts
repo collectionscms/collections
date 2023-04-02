c457813ec14bb42600681b1f25db514324a6c196
kazaneshimizu <ks@rocketa.co.jp>
2023-04-02 15:43:09 +0900

    chore: Added validation of status field.

--- src/admin/fields/schemas/collections/updateCollection.ts
+++ src/admin/fields/schemas/collections/updateCollection.ts
@@ -4,12 +4,38 @@ import yup from '../../yup';
 export type FormValues = {
   hidden: boolean;
   singleton: boolean;
+  statusField: string;
+  draftValue: string;
+  publishValue: string;
+  unpublishValue: string;
 };
 
 export const updateCollection = (): ObjectSchema<FormValues> => {
   return yup.object().shape({
     hidden: yup.boolean(),
     singleton: yup.boolean(),
+    statusField: yup.string(),
+    draftValue: yup
+      .string()
+      .max(60)
+      .when('statusField', {
+        is: (value: string) => value,
+        then: (schema) => schema.required(),
+      }),
+    publishValue: yup
+      .string()
+      .max(60)
+      .when('statusField', {
+        is: (value: string) => value,
+        then: (schema) => schema.required(),
+      }),
+    unpublishValue: yup
+      .string()
+      .max(60)
+      .when('statusField', {
+        is: (value: string) => value,
+        then: (schema) => schema.required(),
+      }),
   });
 };
 
