6fd5b3868ab45b7016acd24bbbd169c22aca5c34
kazaneshimizu <ks@rocketa.co.jp>
2023-04-03 23:26:39 +0900

    refactor: Moved logic code from repository to controller.

--- src/server/repositories/contents.ts
+++ src/server/repositories/contents.ts
@@ -1,64 +1,10 @@
-import { RecordNotFoundException } from '../../shared/exceptions/database/recordNotFound';
-import { AbstractRepositoryOptions, BaseRepository, BaseTransaction } from './base';
-import CollectionsRepository from './collections';
+import { BaseRepository, BaseTransaction } from './base';
 
 export default class ContentsRepository extends BaseRepository<unknown> {
-  appAccess: boolean;
-
-  constructor(collection: string, appAccess: boolean, options?: AbstractRepositoryOptions) {
-    super(collection, options);
-    this.appAccess = appAccess;
-  }
-
   transacting(trx: BaseTransaction): ContentsRepository {
-    const repositoryTransaction = new ContentsRepository(this.collection, this.appAccess, {
+    const repositoryTransaction = new ContentsRepository(this.collection, {
       knex: trx.transaction,
     });
     return repositoryTransaction;
-  }
-
-  async read(data: Partial<unknown> = {}): Promise<unknown[]> {
-    const conditions = await this.makeConditions(data);
-    return super.read(conditions);
-  }
-
-  async readOne(id: number | Partial<unknown>): Promise<unknown> {
-    const conditions = await this.makeConditions({ id });
-
-    const content = (await super.read(conditions))[0];
-    if (!content) throw new RecordNotFoundException('record_not_found');
-
-    return content;
-  }
-
-  async update(id: number, item: Partial<unknown>): Promise<boolean> {
-    const conditions = await this.makeConditions({ id });
-
-    const content = (await super.read(conditions))[0];
-    if (!content) throw new RecordNotFoundException('record_not_found');
-
-    return super.update(id, item);
-  }
-
-  async delete(id: number): Promise<boolean> {
-    const conditions = await this.makeConditions({ id });
-
-    const content = (await super.read(conditions))[0];
-    if (!content) throw new RecordNotFoundException('record_not_found');
-
-    return super.delete(id);
-  }
-
-  // Get the status field and value from the collection.
-  async makeConditions(data: Partial<unknown>) {
-    const collectionsRepository = new CollectionsRepository();
-    const collection = (await collectionsRepository.read({ collection: this.collection }))[0];
-
-    if (!this.appAccess && collection.statusField) {
-      // For Non-application, only public data can be accessed.
-      data[collection.statusField] = collection.publishValue;
-    }
-
-    return data;
   }
 }
