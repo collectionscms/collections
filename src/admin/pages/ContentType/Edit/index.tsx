dd16a724485bdb29c5536e74188bc5c159d3ad45
kazaneshimizu <ks@rocketa.co.jp>
2023-04-11 22:23:26 +0900

    feat: Add Edit Field  to Menu.

--- src/admin/pages/ContentType/Edit/index.tsx
+++ src/admin/pages/ContentType/Edit/index.tsx
@@ -29,14 +29,16 @@ import updateCollectionSchema, {
 } from '../../../fields/schemas/collections/updateCollection';
 import { CollectionContextProvider, useCollection } from '../Context';
 import CreateField from './CreateField';
+import EditField from './EditField';
 import EditMenu from './Menu';
 import { SortableFieldList } from './SortableFieldList';
 
 const EditPage: React.FC = () => {
   const [createFieldOpen, setCreateFieldOpen] = useState(false);
+  const [editFieldOpen, setEditFieldOpen] = useState(false);
   const [menu, setMenu] = useState(null);
-  const [selectedFieldId, setSelectedFieldId] = useState(null);
-  const [sortableFields, setSortableFields] = useState([]);
+  const [selectedField, setSelectedField] = useState<Field>(null);
+  const [sortableFields, setSortableFields] = useState<Field[]>([]);
 
   const { id } = useParams();
   const { t } = useTranslation();
@@ -116,15 +118,24 @@ const EditPage: React.FC = () => {
   // Edit Field
   // /////////////////////////////////////
 
-  const onOpenMenu = (currentTarget: EventTarget, id: number) => {
-    setSelectedFieldId(id);
+  const onOpenMenu = (currentTarget: EventTarget, field: Field) => {
+    setSelectedField(field);
     setMenu(currentTarget);
   };
 
   const onCloseMenu = () => setMenu(null);
 
+  const handleEditField = (open: boolean) => {
+    setEditFieldOpen(open);
+  };
+
+  const handleEditFieldSuccess = (field: Field) => {
+    setEditFieldOpen(false);
+    mutate(fields.map((f) => (f.id === field.id ? field : f)));
+  };
+
   const handleDeleteFieldSuccess = () => {
-    mutate(fields.filter((field) => field.id !== selectedFieldId));
+    mutate(fields.filter((field) => field.id !== selectedField.id));
     onCloseMenu();
   };
 
@@ -136,13 +147,24 @@ const EditPage: React.FC = () => {
         onSuccess={handleCreateFieldSuccess}
         onClose={() => onToggleCreateField(false)}
       />
-      <EditMenu
-        id={selectedFieldId}
-        collectionId={id}
-        menu={menu}
-        onSuccess={handleDeleteFieldSuccess}
-        onClose={onCloseMenu}
-      />
+      {selectedField && (
+        <>
+          <EditField
+            field={selectedField}
+            open={editFieldOpen}
+            onSuccess={handleEditFieldSuccess}
+            onClose={() => handleEditField(false)}
+          />
+          <EditMenu
+            id={selectedField.id.toString()}
+            collectionId={id}
+            menu={menu}
+            onEdit={() => handleEditField(true)}
+            onSuccess={handleDeleteFieldSuccess}
+            onClose={onCloseMenu}
+          />
+        </>
+      )}
       <Stack component="form" onSubmit={handleSubmit(onSubmit)} rowGap={3}>
         <Grid container spacing={2}>
           <Grid xs>
@@ -172,7 +194,7 @@ const EditPage: React.FC = () => {
                       <Box sx={{ flexGrow: 1 }}>{item.field}</Box>
                       {!item.hidden && (
                         <SortableFieldList.ItemMenu
-                          onClickItem={(e) => onOpenMenu(e.currentTarget, item.id)}
+                          onClickItem={(e) => onOpenMenu(e.currentTarget, item)}
                         />
                       )}
                     </Box>
