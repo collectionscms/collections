77061c5da0b3c9ab02b041cf4a9bdcf3ec4b5953
kazaneshimizu <ks@rocketa.co.jp>
2023-04-15 09:02:58 +0900

    feat: Create boolean field validation.

---
+++ src/admin/fields/schemas/collectionFields/boolean/updateBoolean.ts
@@ -0,0 +1,12 @@
+import { ObjectSchema } from 'yup';
+import yup from '../../../yup';
+
+export type FormValues = {
+  label: string;
+  defaultValue: boolean;
+};
+
+export const updateBoolean: ObjectSchema<FormValues> = yup.object().shape({
+  label: yup.string().required().max(60),
+  defaultValue: yup.boolean(),
+});
