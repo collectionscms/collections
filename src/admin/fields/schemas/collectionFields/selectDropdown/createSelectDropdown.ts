d803cb90ba742f8de72752e8c6162ae1adfa984d
kazaneshimizu <ks@rocketa.co.jp>
2023-04-15 11:38:05 +0900

    feat: Create validation for select dropdown field.

---
+++ src/admin/fields/schemas/collectionFields/selectDropdown/createSelectDropdown.ts
@@ -0,0 +1,23 @@
+import { TFunction } from 'i18next';
+import { ObjectSchema } from 'yup';
+import yup from '../../../yup';
+
+export type FormValues = {
+  field: string;
+  label: string;
+  required: boolean;
+  choices: Record<string, any>[];
+};
+
+export const createSelectDropdown = (t: TFunction): ObjectSchema<FormValues> => {
+  return yup.object().shape({
+    field: yup
+      .string()
+      .matches(/^[_0-9a-z]+$/, t('yup.custom.lower_case_alphanumerics_and_underscore'))
+      .required()
+      .max(60),
+    label: yup.string().required().max(60),
+    required: yup.boolean(),
+    choices: yup.array(),
+  });
+};
