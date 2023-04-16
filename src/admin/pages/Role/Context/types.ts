6d7456fdf2036655e9a00f2e5ab4c5221d5f246d
kazaneshimizu <ks@rocketa.co.jp>
2023-04-16 19:25:12 +0900

    refactor: Change implementation to conditionally fetch role.

--- src/admin/pages/Role/Context/types.ts
+++ src/admin/pages/Role/Context/types.ts
@@ -4,7 +4,7 @@ import { Collection, Permission, Role } from '../../../../shared/types';
 
 export type RoleContext = {
   getRoles: () => SWRResponse<Role[]>;
-  getRole: (id: string, config?: SWRConfiguration) => SWRResponse<Role>;
+  getRole: (id: string) => SWRMutationResponse<Role>;
   createRole: () => SWRMutationResponse<Role, any, Record<string, any>, any>;
   updateRole: (id: string) => SWRMutationResponse<void, any, Record<string, any>, any>;
   getCollections: (config?: SWRConfiguration) => SWRResponse<Collection[]>;
