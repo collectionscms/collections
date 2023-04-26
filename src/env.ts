384e9fd60d4f0d3a9fe9e88d3c37860b95928258
kazaneshimizu <ks@rocketa.co.jp>
2023-04-26 09:24:19 +0900

    refactor: Get __dirname from meta.url.

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,10 @@
+import path from 'path';
 import process from 'process';
+import { fileURLToPath } from 'url';
 import { v4 as uuidv4 } from 'uuid';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /* eslint-disable max-len */
 export const defaults: Record<string, any> = {
@@ -92,13 +97,8 @@ export const defaults: Record<string, any> = {
   EMAIL_SENDGRID_API_KEY: '***',
 };
 
-let env: Record<string, any> = {
+export let env: Record<string, any> = {
   ...defaults,
   ...process.env,
-};
-
-export default env;
-
-export const environment = {
-  rootDir: __dirname,
+  ROOT_DIR: __dirname,
 };
