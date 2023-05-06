f73bfca104482020b27ef3c8ec4d1ccd5607431b
kazaneshimizu <ks@rocketa.co.jp>
2023-05-06 22:53:14 +0900

    feat: Create a method to get base64 encoded files.

--- src/server/storages/drivers/LocalFileSystemStorage.ts
+++ src/server/storages/drivers/LocalFileSystemStorage.ts
@@ -3,6 +3,11 @@ import { env } from '../../../env.js';
 import { Storage } from './base.js';
 
 export class LocalFileSystemStorage implements Storage {
+  get(location: string, encoding: BufferEncoding = 'base64'): Promise<string> {
+    const result = fse.readFile(location, encoding);
+    return result;
+  }
+
   put(fileNameDisk: string, content: Buffer): Promise<unknown> {
     const filePath = `./${env.STORAGE_LOCAL_ROOT}/${fileNameDisk}`;
     const result = fse.outputFile(filePath, content);
--- src/server/storages/drivers/base.ts
+++ src/server/storages/drivers/base.ts
@@ -1,7 +1,9 @@
 type AbstractStorage = {
+  get(location: string, encoding?: BufferEncoding): Promise<string>;
   put(fileNameDisk: string, content: Buffer): Promise<unknown>;
 };
 
 export abstract class Storage implements AbstractStorage {
+  abstract get(location: string, encoding?: BufferEncoding): Promise<string>;
   abstract put(fileNameDisk: string, content: Buffer): Promise<unknown>;
 }
