13f14ee68f993e533532bce632ae827b8754e25f
kazaneshimizu <ks@rocketa.co.jp>
2023-05-07 10:50:29 +0900

    feat: Create API for file fetch.

--- src/server/controllers/files.ts
+++ src/server/controllers/files.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, raw } from 'express';
+import express, { Request, Response } from 'express';
 import { pathList } from '../../utilities/pathList.js';
 import { asyncHandler } from '../middleware/asyncHandler.js';
 import { multipartHandler } from '../middleware/multipartHandler.js';
@@ -19,11 +19,25 @@ app.post(
     const file = await repository.readOne(keys[0]);
 
     const path = pathList.storageRoot(file.fileNameDisk);
+    const raw = await getStorage().get(path);
 
-    const storage = getStorage();
-    const raw = await storage.get(path);
+    res.json({ file, raw });
+  })
+);
 
-    res.status(200).json({ file, raw });
+app.get(
+  '/files/:id',
+  permissionsHandler([{ collection: 'superfast_files', action: 'read' }]),
+  asyncHandler(async (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    const repository = new FilesRepository();
+    const file = await repository.readOne(id);
+
+    const path = pathList.storageRoot(file.fileNameDisk);
+    const raw = await getStorage().get(path);
+
+    res.json({ file, raw });
   })
 );
 
