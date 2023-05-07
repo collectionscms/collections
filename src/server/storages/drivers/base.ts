c085ecc2b556c3ca20cf32a5b476a16ce7df831e
kazaneshimizu <ks@rocketa.co.jp>
2023-05-07 17:33:59 +0900

    docs: Add method comments.

--- src/server/storages/drivers/base.ts
+++ src/server/storages/drivers/base.ts
@@ -4,6 +4,9 @@ type AbstractStorage = {
 };
 
 export abstract class Storage implements AbstractStorage {
+  // Returns the raw data of the file. Encoding can be selected from base64, utf8, etc.
   abstract get(location: string, encoding?: BufferEncoding): Promise<string>;
+
+  // Save the file to a storage location. The data returned depends on the selected storage driver.
   abstract put(fileNameDisk: string, content: Buffer): Promise<unknown>;
 }
