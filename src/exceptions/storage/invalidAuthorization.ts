d65f7390ec5b4d45d4220eaa0fccff2f2dfffe8e
kazaneshimizu <ks@rocketa.co.jp>
2023-05-08 22:43:39 +0900

    feat: Create storage exceptions.

---
+++ src/exceptions/storage/invalidAuthorization.ts
@@ -0,0 +1,7 @@
+import { BaseException } from '../base.js';
+
+export class InvalidAuthorizationException extends BaseException {
+  constructor(public code: string, public extensions?: Record<string, any>) {
+    super(400, code, extensions);
+  }
+}
