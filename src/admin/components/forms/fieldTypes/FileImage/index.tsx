b633aa4a872253243db2598a1166e47c7f67e462
kazaneshimizu <ks@rocketa.co.jp>
2023-05-09 08:31:05 +0900

    refactor: Change request processing to swr.

--- src/admin/components/forms/fieldTypes/FileImage/index.tsx
+++ src/admin/components/forms/fieldTypes/FileImage/index.tsx
@@ -2,7 +2,9 @@ import { PhotoCamera } from '@mui/icons-material';
 import { Box, IconButton } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import useSWRMutation, { SWRMutationResponse } from 'swr/mutation';
 import { File } from '../../../../../config/types.js';
+import { logger } from '../../../../../utilities/logger.js';
 import { api } from '../../../../utilities/api.js';
 import { Props } from '../types.js';
 
@@ -13,31 +15,61 @@ export const FileImageType: React.FC<Props> = ({
   const { t } = useTranslation();
   const required = meta.required && { required: t('yup.mixed.required') };
   const [content, setContent] = useState<string | null>(null);
-  const [fileId, setFileId] = useState<number | null>(null);
+  const [fileId, setFileId] = useState<string | null>(null);
+
+  const getFileImage = (id: string | null): SWRMutationResponse<{ file: File; raw: string }> =>
+    useSWRMutation(id ? `/files/${id}` : null, (url) =>
+      api.get<{ file: File; raw: string }>(url).then((res) => res.data)
+    );
+
+  const createFileImage = () =>
+    useSWRMutation(`/files`, async (url: string, { arg }: { arg: Record<string, any> }) => {
+      return api.post<{ file: File; raw: string }>(url, arg).then((res) => res.data);
+    });
+
+  const { trigger: getFileImageTrigger } = getFileImage(fileId);
+  const { trigger: createFileImageTrigger } = createFileImage();
 
   useEffect(() => {
-    const getFile = async (fileId: string) => {
-      const res = await api.get<{ file: File; raw: string }>(`/files/${fileId}`);
-      setContent(`data:${res.data.file.type};base64,${res.data.raw}`);
-      setValue(meta.field, res.data.file.id);
-      setFileId(res.data.file.id);
-    };
-
     const watchId = watch(meta.field);
     if (watchId && watchId !== fileId) {
-      getFile(watchId);
+      setFileId(watchId);
     }
   }, [watch(meta.field)]);
 
+  useEffect(() => {
+    const setFileImage = async () => {
+      try {
+        const res = await getFileImageTrigger();
+        if (res) {
+          setContent(`data:${res.file.type};base64,${res.raw}`);
+          setValue(meta.field, res.file.id);
+        }
+      } catch (e) {
+        logger.error(e);
+      }
+    };
+
+    if (fileId) {
+      setFileImage();
+    }
+  }, [fileId]);
+
   const onSelectedFile = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-    if (file) {
-      const params = new FormData();
-      params.append('image', file);
-      const res = await api.post<{ file: File; raw: string }>('/files', params);
-      setContent(`data:${res.data.file.type};base64,${res.data.raw}`);
-      setValue(meta.field, res.data.file.id);
-      setFileId(res.data.file.id);
+    if (!file) return;
+
+    const params = new FormData();
+    params.append('image', file);
+
+    try {
+      const res = await createFileImageTrigger(params);
+      if (res) {
+        setContent(`data:${res.file.type};base64,${res.raw}`);
+        setValue(meta.field, res.file.id);
+      }
+    } catch (e) {
+      logger.error(e);
     }
   };
 
