c7a399f094cabc0f29adb5a1e933cdabdc929aa7
kazaneshimizu <ks@rocketa.co.jp>
2023-05-08 22:44:44 +0900

    feat: Create AWS S3 storage driver.

---
+++ src/server/storages/drivers/AwsS3Storage.ts
@@ -0,0 +1,73 @@
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  PutObjectCommandOutput,
+  S3Client,
+} from '@aws-sdk/client-s3';
+import { env } from '../../../env.js';
+import { InvalidAuthorizationException } from '../../../exceptions/storage/invalidAuthorization.js';
+import { InvalidSecretKeyException } from '../../../exceptions/storage/invalidSecretKey.js';
+import { UnknownException } from '../../../exceptions/storage/unknown.js';
+import { Storage } from './base.js';
+
+export class AwsS3Storage implements Storage {
+  key(fileNameDisk: string): string {
+    return fileNameDisk;
+  }
+
+  async get(key: string, encoding: BufferEncoding = 'base64'): Promise<string> {
+    try {
+      const result = await this.client().send(
+        new GetObjectCommand({
+          Bucket: env.STORAGE_BUCKET,
+          Key: key,
+        })
+      );
+      const obj = result.Body?.transformToString(encoding);
+      return obj ?? '';
+    } catch (e: any) {
+      throw this.handleError(e, key, env.STORAGE_BUCKET);
+    }
+  }
+
+  async put(fileNameDisk: string, content: Buffer): Promise<PutObjectCommandOutput> {
+    try {
+      const output = await this.client().send(
+        new PutObjectCommand({
+          Bucket: env.STORAGE_BUCKET,
+          Key: fileNameDisk,
+          Body: content,
+        })
+      );
+      return output;
+    } catch (e: any) {
+      throw this.handleError(e, fileNameDisk, env.STORAGE_BUCKET);
+    }
+  }
+
+  client = () => {
+    return new S3Client({
+      region: env.STORAGE_REGION,
+      credentials: {
+        accessKeyId: env.STORAGE_KEY,
+        secretAccessKey: env.STORAGE_SECRET,
+      },
+    });
+  };
+
+  // see: https://docs.aws.amazon.com/AmazonS3/latest/API/ErrorResponses.html
+  handleError(e: Error, key: string, bucket: string): Error {
+    const extension = {
+      message: `key: ${key}, bucket: ${bucket}, error name: ${e.name}, error msg: ${e.message}`,
+    };
+
+    switch (e.name) {
+      case 'AuthorizationHeaderMalformed':
+        return new InvalidAuthorizationException('invalid_access_key', extension);
+      case 'SignatureDoesNotMatch':
+        return new InvalidSecretKeyException('invalid_secret_key', extension);
+      default:
+        return new UnknownException('internal_server_error', extension);
+    }
+  }
+}
--- src/server/storages/storage.ts
+++ src/server/storages/storage.ts
@@ -1,6 +1,11 @@
+import { AwsS3Storage } from './drivers/AwsS3Storage.js';
 import { LocalFileSystemStorage } from './drivers/LocalFileSystemStorage.js';
 import { Storage } from './drivers/base.js';
 
-export const getStorage = (): Storage => {
+export const getStorage = (driver: string): Storage => {
+  if (driver === 'aws-s3') {
+    return new AwsS3Storage();
+  }
+
   return new LocalFileSystemStorage();
 };
