72f5c3efd44ee459b7d24783d99af22908395092
kazaneshimizu <ks@rocketa.co.jp>
2023-05-11 22:45:49 +0900

    feat: Create a method that returns binary data.

--- src/server/storages/drivers/base.ts
+++ src/server/storages/drivers/base.ts
@@ -1,6 +1,7 @@
 type AbstractStorage = {
   key(fileNameDisk: string): string;
   get(key: string, encoding?: BufferEncoding): Promise<string>;
+  getBuffer(location: string): Promise<Buffer>;
   put(fileNameDisk: string, content: Buffer): Promise<unknown>;
 };
 
@@ -10,6 +11,9 @@ export abstract class Storage implements AbstractStorage {
 
   // Returns the raw data of the file. Encoding can be selected from base64, utf8, etc.
   abstract get(key: string, encoding?: BufferEncoding): Promise<string>;
+
+  // Returns the binary data of the file.
+  abstract getBuffer(location: string): Promise<Buffer>;
 
   // Save the file to a storage location. The data returned depends on the selected storage driver.
   abstract put(fileNameDisk: string, content: Buffer): Promise<unknown>;
