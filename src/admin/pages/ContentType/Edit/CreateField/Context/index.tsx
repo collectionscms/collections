948417901dd8de9785466bf90fe7c6eeb69c624b
kazaneshimizu <ks@rocketa.co.jp>
2023-05-23 06:53:46 +0900

    feat: Add getCollections method.

--- src/admin/pages/ContentType/Edit/CreateField/Context/index.tsx
+++ src/admin/pages/ContentType/Edit/CreateField/Context/index.tsx
@@ -1,8 +1,9 @@
 import React, { createContext, useContext, useMemo } from 'react';
 import useSWRMutation from 'swr/mutation';
-import { Field } from '../../../../../../config/types.js';
+import { Collection, Field } from '../../../../../../config/types.js';
 import { api } from '../../../../../utilities/api.js';
 import { FieldContext } from './types.js';
+import useSWR, { SWRResponse } from 'swr';
 
 const Context = createContext({} as FieldContext);
 
@@ -12,11 +13,17 @@ export const FieldContextProvider: React.FC<{ children: React.ReactNode }> = ({ 
       return api.post<{ field: Field }>(url, arg).then((res) => res.data.field);
     });
 
+  const getCollections = (): SWRResponse =>
+    useSWR('/collections', (url) =>
+      api.get<{ collections: Collection[] }>(url).then((res) => res.data.collections)
+    );
+
   const value = useMemo(
     () => ({
       createField,
+      getCollections,
     }),
-    [createField]
+    [createField, getCollections]
   );
 
   return <Context.Provider value={value}>{children}</Context.Provider>;
--- src/admin/pages/ContentType/Edit/CreateField/Context/types.ts
+++ src/admin/pages/ContentType/Edit/CreateField/Context/types.ts
@@ -1,6 +1,8 @@
 import { SWRMutationResponse } from 'swr/mutation';
-import { Field } from '../../../../../../config/types.js';
+import { Collection, Field } from '../../../../../../config/types.js';
+import { SWRResponse } from 'swr';
 
 export type FieldContext = {
   createField: () => SWRMutationResponse<Field, any, Record<string, any>, any>;
+  getCollections: () => SWRResponse<Collection[]>;
 };
