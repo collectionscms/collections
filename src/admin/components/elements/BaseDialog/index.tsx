ccd894abc9719b9b946bd8371f0c538588078363
kazaneshimizu <ks@rocketa.co.jp>
2023-06-04 20:17:52 +0900

    feat: Create base dialog component.

---
+++ src/admin/components/elements/BaseDialog/index.tsx
@@ -0,0 +1,34 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+import React from 'react';
+import { Props } from './types.js';
+
+export const BaseDialog: React.FC<Props> = ({ open, title, body, confirm, cancel }) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={cancel.action}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+      <DialogContent id="alert-dialog-description">
+        <DialogContentText>{body}</DialogContentText>
+      </DialogContent>
+      <DialogActions sx={{ px: 3, pb: 2 }}>
+        <Button onClick={cancel.action} autoFocus>
+          {cancel.label}
+        </Button>
+        <Button variant="contained" onClick={confirm.action}>
+          {confirm.label}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
