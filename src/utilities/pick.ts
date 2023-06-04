18e0c00442a36d460502e02a10be7727031fee20
kazaneshimizu <ks@rocketa.co.jp>
2023-06-04 10:37:20 +0900

    fix: Refactor the implementation.

--- src/utilities/pick.ts
+++ src/utilities/pick.ts
@@ -1,10 +1,11 @@
 // This function returns a new object with only the specified keys picked from the original object.
 // Alternatives to lodash _.pick.
 // ref: https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_pick
-export function pick(obj: any, ...keys: string[][]) {
-  const ret: Record<string, any> = {};
-  keys.flat().forEach((key) => {
-    if (obj[key]) ret[key] = obj[key];
-  });
-  return ret;
+export function pick(object: any, keys: string[]) {
+  return keys.reduce((acc: Record<string, any>, key) => {
+    if (object && object.hasOwnProperty(key)) {
+      acc[key] = object[key];
+    }
+    return acc;
+  }, {});
 }
