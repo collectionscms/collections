0c9507786d1ab6bbb911be485f1408206bc77025
kazaneshimizu <ks@rocketa.co.jp>
2023-06-11 16:24:10 +0900

    refactor: Call logout API to delete cookies.

--- src/admin/pages/Logout/index.tsx
+++ src/admin/pages/Logout/index.tsx
@@ -1,14 +1,24 @@
 import { Button, Stack } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { logger } from '../../../utilities/logger.js';
 import { RouterLink } from '../../components/elements/Link/index.js';
 import { useAuth } from '../../components/utilities/Auth/index.js';
 
 export const Logout: React.FC = () => {
   const { t } = useTranslation();
   const { logout } = useAuth();
+  const { trigger } = logout();
 
   useEffect(() => {
+    const logout = async () => {
+      try {
+        await trigger();
+      } catch (e) {
+        logger.error(e);
+      }
+    };
+
     logout();
   }, []);
 
