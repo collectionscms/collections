ef0409b01155cd2c77cc8b04b0d9a36002839961
kazaneshimizu <ks@rocketa.co.jp>
2023-06-11 09:59:48 +0900

    refactor: Add getting token from cookies.

--- src/server/middleware/extractTokenHandler.ts
+++ src/server/middleware/extractTokenHandler.ts
@@ -1,24 +1,35 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
+import { env } from '../../env.js';
+import { parseCookies } from '../utilities/parseCookies.js';
 
 export const extractTokenHandler: RequestHandler = (
   req: Request,
-  res: Response,
+  _res: Response,
   next: NextFunction
 ) => {
-  let token: string | null = null;
-
+  // Extract token from query string, ?access_token=123
   if (req.query && req.query.access_token) {
-    token = req.query.access_token as string;
+    req.token = req.query.access_token as string;
+    return next();
   }
 
+  // Extract token from authentication headers, Bearer <token>
   if (req.headers && req.headers.authorization) {
     const parts = req.headers.authorization.split(' ');
 
     if (parts.length === 2 && parts[0].toLowerCase() === 'bearer') {
-      token = parts[1];
+      req.token = parts[1];
+      return next();
     }
   }
 
-  req.token = token;
+  // Extract token from cookies
+  const cookies = parseCookies(req);
+  const tokenCookieName = `${env.COOKIE_PREFIX}-token`;
+
+  if (cookies && cookies[tokenCookieName]) {
+    req.token = cookies[tokenCookieName];
+  }
+
   next();
 };
