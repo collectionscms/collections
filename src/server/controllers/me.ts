e8805378acebf9976f3e0fe9602ada36d3055a88
kazaneshimizu <ks@rocketa.co.jp>
2023-06-11 16:08:54 +0900

    refactor: If unauthenticated, extract jwt from the cookie.

--- src/server/controllers/me.ts
+++ src/server/controllers/me.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import { InvalidCredentialsException } from '../../exceptions/invalidCredentials.js';
 import { asyncHandler } from '../middleware/asyncHandler.js';
 import { UsersRepository } from '../repositories/users.js';
 import { decodeJwt } from '../utilities/decodeJwt.js';
+import { getExtractJwt } from '../utilities/getExtractJwt.js';
 import { sign } from '../utilities/sign.js';
 
 const router = express.Router();
@@ -11,20 +13,30 @@ router.get(
   asyncHandler(async (req: Request, res: Response) => {
     const repository = new UsersRepository();
 
-    const user = await repository.readMe({ id: Number(req.userId) });
-    if (!user) {
-      return res.json({ user: null, token: null, exp: 0 });
+    if (req.userId) {
+      const user = await repository.readMe({ id: Number(req.userId) });
+      if (!user) {
+        throw new InvalidCredentialsException('token_invalid_or_expired');
+      }
+      user.appAccess = true;
+
+      const token = sign(user);
+      const decoded = decodeJwt(token);
+
+      return res.json({
+        user,
+        token,
+        exp: decoded?.exp || 0,
+      });
     }
-    user.appAccess = true;
 
-    const token = sign(user);
-    const decoded = decodeJwt(token);
+    const token = getExtractJwt(req);
+    if (token) {
+      const decoded = decodeJwt(token);
+      return res.json({ user: decoded, token: token, exp: decoded?.exp || 0 });
+    }
 
-    res.json({
-      user,
-      token,
-      exp: decoded?.exp || 0,
-    });
+    res.json({ user: null, token: null, exp: 0 });
   })
 );
 
