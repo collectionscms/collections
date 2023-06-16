78dc587e4bec6e388f0121f26d95e2cf1ce55654
kazaneshimizu <ks@rocketa.co.jp>
2023-06-16 22:05:40 +0900

    refactor: Add expiration date to parameter.

--- src/server/utilities/sign.ts
+++ src/server/utilities/sign.ts
@@ -2,8 +2,8 @@ import jwt from 'jsonwebtoken';
 import { AuthUser } from '../../config/types.js';
 import { env } from '../../env.js';
 
-export const sign = (user: AuthUser): string => {
+export const sign = (user: AuthUser, expiresIn: number): string => {
   return jwt.sign(user, env.SECRET, {
-    expiresIn: env.ACCESS_TOKEN_TTL,
+    expiresIn,
   });
 };
