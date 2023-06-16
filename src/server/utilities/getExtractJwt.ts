eef8a0bccd92b7a1a21a0fb877df2b1e175d8712
kazaneshimizu <ks@rocketa.co.jp>
2023-06-16 22:12:35 +0900

    refactor: Change cookie name.

--- src/server/utilities/getExtractJwt.ts
+++ src/server/utilities/getExtractJwt.ts
@@ -5,7 +5,7 @@ import { parseCookies } from './parseCookies.js';
 // Extract token from cookies
 export const getExtractJwt = (req: Request): string | null => {
   const cookies = parseCookies(req);
-  const tokenCookieName = `${env.COOKIE_PREFIX}-token`;
+  const tokenCookieName = `${env.COOKIE_PREFIX}-refresh-token`;
 
   if (cookies && cookies[tokenCookieName]) {
     return cookies[tokenCookieName];
