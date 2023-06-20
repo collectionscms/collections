5ab24aa859b3c5345d3026316ba8680561107431
kazaneshimizu <ks@rocketa.co.jp>
2023-06-20 22:33:50 +0900

    refactor: Pass connection information as a parameter.

--- src/server/database/connection.ts
+++ src/server/database/connection.ts
@@ -8,15 +8,38 @@ const __dirname = path.dirname(__filename);
 
 let database: Knex | null = null;
 
-export const getDatabase = (): Knex => {
+export type Credentials = {
+  filename?: string;
+  host?: string;
+  port?: number;
+  database?: string;
+  user?: string;
+  password?: string;
+};
+
+/**
+ * Get a database connection.
+ *
+ * @param client
+ * @param credentials
+ * @returns
+ */
+export const getDatabase = (client: 'sqlite3' | 'mysql' | 'pg', credentials: Credentials): Knex => {
   if (database) return database;
   const migrationFiles = path.join(__dirname, 'migrations');
+  let connection: Knex.Config['connection'] = {};
+
+  if (client === 'sqlite3') {
+    connection = {
+      filename: credentials.filename || env.DB_FILENAME,
+    };
+  } else {
+    connection = { ...credentials };
+  }
 
   const config: Knex.Config = {
-    client: env.DB_CLIENT,
-    connection: {
-      filename: env.DB_FILENAME,
-    },
+    client: client,
+    connection,
     useNullAsDefault: true,
     migrations: {
       directory: migrationFiles,
