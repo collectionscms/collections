5485d054708afc29933e4e57d8e0563a80671e73
kazaneshimizu <ks@rocketa.co.jp>
2023-07-03 05:58:55 +0900

    style: site color definition

--- src/types/createPalette.d.ts
+++ src/types/createPalette.d.ts
@@ -1,3 +1,5 @@
+import * as createPalette from '@mui/material/styles';
+
 export declare module '@mui/material/styles' {
   interface Palette {
     sidebar: Palette['primary'];
@@ -7,6 +9,52 @@ export declare module '@mui/material/styles' {
   interface PaletteOptions {
     sidebar?: PaletteOptions['primary'];
     sidebarIcon?: PaletteOptions['primary'];
+  }
+
+  interface SimplePaletteColorOptions {
+    lighter?: string;
+    darker?: string;
+    0?: string;
+    50?: string;
+    100?: string;
+    200?: string;
+    300?: string;
+    400?: string;
+    500?: string;
+    600?: string;
+    700?: string;
+    800?: string;
+    900?: string;
+    A50?: string;
+    A100?: string;
+    A200?: string;
+    A300?: string;
+    A400?: string;
+    A700?: string;
+    A800?: string;
+  }
+
+  interface PaletteColor {
+    lighter: string;
+    darker: string;
+    0?: string;
+    50?: string;
+    100?: string;
+    200?: string;
+    300?: string;
+    400?: string;
+    500?: string;
+    600?: string;
+    700?: string;
+    800?: string;
+    900?: string;
+    A50?: string;
+    A100?: string;
+    A200?: string;
+    A300?: string;
+    A400?: string;
+    A700?: string;
+    A800?: string;
   }
 }
 
---
+++ src/types/mui/index.d.ts
@@ -0,0 +1,9 @@
+import * as Color from '@mui/material';
+
+declare module '@mui/material' {
+  interface Color {
+    0?: string;
+    A50?: string;
+    A800?: string;
+  }
+}
