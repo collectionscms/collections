b2bddd8acd2f317594a09f88e221a37f1cd28e40
kazaneshimizu <ks@rocketa.co.jp>
2023-07-03 06:00:21 +0900

    style: create dark mode color palette

--- src/admin/themes/Dark/palette.ts
+++ src/admin/themes/Dark/palette.ts
@@ -1,14 +1,46 @@
-import { PaletteOptions } from '@mui/material';
-import { colorBase } from '../colors.js';
+import { PalettesProps, presetDarkPalettes } from '@ant-design/colors';
+import { PaletteOptions, alpha } from '@mui/material';
+import { Theme } from './theme.js';
+
+const greyPrimary = [
+  '#000000',
+  '#141414',
+  '#1e1e1e',
+  '#595959',
+  '#8c8c8c',
+  '#bfbfbf',
+  '#d9d9d9',
+  '#f0f0f0',
+  '#f5f5f5',
+  '#fafafa',
+  '#ffffff',
+];
+const greyAscent = ['#fafafa', '#bfbfbf', '#434343', '#1f1f1f'];
+const greyConstant = ['#121212', '#d3d8db'];
+
+const colors: PalettesProps = presetDarkPalettes;
+colors.grey = [...greyPrimary, ...greyAscent, ...greyConstant];
+
+const paletteColor = Theme(colors);
 
 export const palette: PaletteOptions = {
   mode: 'dark',
-  primary: { main: colorBase.color_base_0 },
-  secondary: { main: '#eaff96' },
-  background: {
-    default: colorBase.color_base_900,
-    paper: colorBase.color_base_900,
+  common: {
+    black: '#000',
+    white: '#fff',
   },
-  sidebar: { main: colorBase.color_base_800 },
-  sidebarIcon: { main: colorBase.color_base_400 },
+  ...paletteColor,
+  text: {
+    primary: alpha(paletteColor.grey[900]!, 0.87),
+    secondary: alpha(paletteColor.grey[900]!, 0.45),
+    disabled: alpha(paletteColor.grey[900]!, 0.1),
+  },
+  action: {
+    disabled: paletteColor.grey[300],
+  },
+  divider: alpha(paletteColor.grey[900]!, 0.05),
+  background: {
+    paper: paletteColor.grey[100],
+    default: paletteColor.grey.A50,
+  },
 };
