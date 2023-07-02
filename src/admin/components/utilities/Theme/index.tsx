d210e97f4d6b16cc74bcd1a59f029d947bd9ba77
kazaneshimizu <ks@rocketa.co.jp>
2023-07-02 17:39:13 +0900

    style: customizing shadow

--- src/admin/components/utilities/Theme/index.tsx
+++ src/admin/components/utilities/Theme/index.tsx
@@ -2,17 +2,48 @@ import {
   CssBaseline,
   ThemeProvider as MuiThemeProvider,
   StyledEngineProvider,
+  Theme,
+  ThemeOptions,
+  createTheme,
 } from '@mui/material';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { themeDark } from '../../../themes/Dark/index.js';
-import { theme } from '../../../themes/Default/index.js';
+import { theme as themeLight } from '../../../themes/Default/index.js';
 import { componentsOverrides } from '../../../themes/overrides/index.js';
+import { CustomShadowProps, CustomShadows } from '../../../themes/shadows.js';
 import { useColorMode } from '../ColorMode/index.js';
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { mode } = useColorMode();
-  const themes = mode === 'light' ? theme : themeDark;
+  const theme: Theme = mode === 'light' ? themeLight : themeDark;
 
+  const customShadows = useMemo<CustomShadowProps>(() => CustomShadows(mode, theme), [theme]);
+
+  const themeOptions: ThemeOptions = useMemo(
+    () => ({
+      breakpoints: {
+        values: {
+          xs: 0,
+          sm: 768,
+          md: 1024,
+          lg: 1266,
+          xl: 1440,
+        },
+      },
+      mixins: {
+        toolbar: {
+          minHeight: 60,
+          paddingTop: 8,
+          paddingBottom: 8,
+        },
+      },
+      palette: theme.palette,
+      customShadows: customShadows,
+    }),
+    [theme, customShadows]
+  );
+
+  const themes = createTheme(themeOptions);
   themes.components = componentsOverrides(themes);
 
   return (
---
+++ src/admin/themes/shadows.ts
@@ -0,0 +1,45 @@
+import { Theme, alpha } from '@mui/material/styles';
+import { Mode } from '../../types/config.js';
+
+export type CustomShadowProps = {
+  button: string;
+  text: string;
+  z1: string;
+  primary: string;
+  primaryButton: string;
+  secondary: string;
+  secondaryButton: string;
+  error: string;
+  errorButton: string;
+  warning: string;
+  warningButton: string;
+  info: string;
+  infoButton: string;
+  success: string;
+  successButton: string;
+  grey: string;
+  greyButton: string;
+};
+
+export const CustomShadows = (mode: Mode, theme: Theme): CustomShadowProps => ({
+  button: mode === 'dark' ? `0 2px 0 rgb(0 0 0 / 5%)` : `0 2px #0000000b`,
+  text: `0 -1px 0 rgb(0 0 0 / 12%)`,
+  z1:
+    mode === 'light'
+      ? `0px 1px 1px rgb(0 0 0 / 14%), 0px 2px 1px rgb(0 0 0 / 12%), 0px 1px 3px rgb(0 0 0 / 20%)`
+      : `0px 1px 4px ${alpha(theme.palette.grey[900], 0.08)}`,
+  primary: `0 0 0 2px ${alpha(theme.palette.primary.main, 0.2)}`,
+  secondary: `0 0 0 2px ${alpha(theme.palette.secondary.main, 0.2)}`,
+  error: `0 0 0 2px ${alpha(theme.palette.error.main, 0.2)}`,
+  warning: `0 0 0 2px ${alpha(theme.palette.warning.main, 0.2)}`,
+  info: `0 0 0 2px ${alpha(theme.palette.info.main, 0.2)}`,
+  success: `0 0 0 2px ${alpha(theme.palette.success.main, 0.2)}`,
+  grey: `0 0 0 2px ${alpha(theme.palette.grey[500], 0.2)}`,
+  primaryButton: `0 14px 12px ${alpha(theme.palette.primary.main, 0.2)}`,
+  secondaryButton: `0 14px 12px ${alpha(theme.palette.secondary.main, 0.2)}`,
+  errorButton: `0 14px 12px ${alpha(theme.palette.error.main, 0.2)}`,
+  warningButton: `0 14px 12px ${alpha(theme.palette.warning.main, 0.2)}`,
+  infoButton: `0 14px 12px ${alpha(theme.palette.info.main, 0.2)}`,
+  successButton: `0 14px 12px ${alpha(theme.palette.success.main, 0.2)}`,
+  greyButton: `0 14px 12px ${alpha(theme.palette.grey[500], 0.2)}`,
+});
