43cfef0e545d04fae203f688ebb97b588172a4f7
kazaneshimizu <ks@rocketa.co.jp>
2023-07-02 16:43:49 +0900

    chore: fix import path

--- src/admin/components/utilities/Theme/index.tsx
+++ src/admin/components/utilities/Theme/index.tsx
@@ -1,9 +1,13 @@
-import { CssBaseline, ThemeProvider as MuiThemeProvider } from '@mui/material';
+import {
+  CssBaseline,
+  ThemeProvider as MuiThemeProvider,
+  StyledEngineProvider,
+} from '@mui/material';
 import React from 'react';
+import { themeDark } from '../../../themes/Dark/index.js';
+import { theme } from '../../../themes/Default/index.js';
+import { componentsOverrides } from '../../../themes/overrides/index.js';
 import { useColorMode } from '../ColorMode/index.js';
-import { themeDark } from './Dark/index.js';
-import { theme } from './Default/index.js';
-import { componentsOverrides } from './overrides/index.js';
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { mode } = useColorMode();
@@ -12,10 +16,12 @@ export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ childre
   themes.components = componentsOverrides(themes);
 
   return (
-    <MuiThemeProvider theme={themes}>
-      <CssBaseline />
-      {children}
-    </MuiThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <MuiThemeProvider theme={themes}>
+        <CssBaseline />
+        {children}
+      </MuiThemeProvider>
+    </StyledEngineProvider>
   );
 };
 
