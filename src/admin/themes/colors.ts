de2f96e644432e8c2640c57e3c16119b834bad80
kazaneshimizu <ks@rocketa.co.jp>
2023-07-03 09:11:36 +0900

    refactor: get theme colors

--- src/admin/themes/colors.ts
+++ src/admin/themes/colors.ts
@@ -1,14 +1,19 @@
-export const colorBase = {
-  color_base_0: '#fff',
-  color_base_50: '#f5f5f5',
-  color_base_100: '#ebebeb',
-  color_base_200: '#d0d0d0',
-  color_base_300: '#b5b5b5',
-  color_base_400: '#9a9a9a',
-  color_base_500: '#808080',
-  color_base_600: '#656565',
-  color_base_700: '#4a4a4a',
-  color_base_800: '#2f2f2f',
-  color_base_900: '#141414',
-  color_base_1000: '#000',
+import { Theme } from '@mui/material';
+import { ColorProps } from '../../types/mui/style.js';
+
+export const getColors = (theme: Theme, color?: ColorProps) => {
+  switch (color!) {
+    case 'secondary':
+      return theme.palette.secondary;
+    case 'error':
+      return theme.palette.error;
+    case 'warning':
+      return theme.palette.warning;
+    case 'info':
+      return theme.palette.info;
+    case 'success':
+      return theme.palette.success;
+    default:
+      return theme.palette.primary;
+  }
 };
