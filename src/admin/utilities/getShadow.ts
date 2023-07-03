0161117efe961c0dc12f5c3589d4ebca3ee31fb8
kazaneshimizu <ks@rocketa.co.jp>
2023-07-04 05:48:19 +0900

    style: create get shadow

---
+++ src/admin/utilities/getShadow.ts
@@ -0,0 +1,30 @@
+import { Theme } from '@mui/material';
+
+export const getShadow = (theme: Theme, shadow: string) => {
+  switch (shadow) {
+    case 'secondary':
+      return theme.customShadows.secondary;
+    case 'error':
+      return theme.customShadows.error;
+    case 'warning':
+      return theme.customShadows.warning;
+    case 'info':
+      return theme.customShadows.info;
+    case 'success':
+      return theme.customShadows.success;
+    case 'primaryButton':
+      return theme.customShadows.primaryButton;
+    case 'secondaryButton':
+      return theme.customShadows.secondaryButton;
+    case 'errorButton':
+      return theme.customShadows.errorButton;
+    case 'warningButton':
+      return theme.customShadows.warningButton;
+    case 'infoButton':
+      return theme.customShadows.infoButton;
+    case 'successButton':
+      return theme.customShadows.successButton;
+    default:
+      return theme.customShadows.primary;
+  }
+};
