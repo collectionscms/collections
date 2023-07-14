d9bd43541691c5c2cb9b002c75b262406e46113b
kazaneshimizu <ks@rocketa.co.jp>
2023-07-15 07:19:50 +0900

    style: restyling not found page

--- src/admin/components/Routes/NoRoutes/index.tsx
+++ src/admin/components/Routes/NoRoutes/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Loader } from '../../elements/Loader/index.js';
 import lazy from '../../../utilities/lazy.js';
+import { Loader } from '../../elements/Loader/index.js';
 
-const MinimalLayout = Loader(lazy(() => import('../../layouts/Minimal/index.js'), 'MinimalLayout'));
 const NotFound = Loader(lazy(() => import('../../../pages/NotFound/index.js'), 'NotFound'));
 
 export const NoRoutes = () => {
   return {
     path: '/admin',
-    element: <MinimalLayout />,
     children: [{ path: '*', element: <NotFound /> }],
   };
 };
