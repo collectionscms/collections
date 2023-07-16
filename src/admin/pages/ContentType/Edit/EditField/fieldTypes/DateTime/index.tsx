aa2d4bc4244d09208cc7b923ae07094f94d174fb
kazaneshimizu <ks@rocketa.co.jp>
2023-07-16 10:38:39 +0900

    style: restyling the content type edit drawer, add custom scrollbar

--- src/admin/pages/ContentType/Edit/EditField/fieldTypes/DateTime/index.tsx
+++ src/admin/pages/ContentType/Edit/EditField/fieldTypes/DateTime/index.tsx
@@ -72,7 +72,7 @@ export const DateTimeType: React.FC<Props> = (props) => {
 
   return (
     <>
-      <Stack component="form" onSubmit={handleSubmit(onSubmit)} sx={{ p: 2 }}>
+      <Stack component="form" onSubmit={handleSubmit(onSubmit)}>
         <Stack rowGap={3}>
           <Grid container spacing={3} columns={{ xs: 1, sm: 4 }}>
             <Grid xs={1} sm={2}>
