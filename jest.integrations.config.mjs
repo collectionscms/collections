4877ebc55f737a01da93c4002a18ab07cd034963
kazaneshimizu <ks@rocketa.co.jp>
2023-07-20 09:25:10 +0900

    test: create integration tests

---
+++ jest.integrations.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  testMatch: ['**/test/server/**/*test.ts'],
+  globalSetup: './test/setups/setup.ts',
+  globalTeardown: './test/setups/teardown.ts',
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
--- test/utilities/testDatabases.ts
+++ test/utilities/testDatabases.ts
@@ -1,2 +1,2 @@
 export const allDatabases = ['sqlite3'];
-export const testDatabases = process.env.TEST_DB?.split(',').map((v) => v.trim()) ?? [];
+export const testDatabases = process.env.TEST_DB?.split(',').map((v) => v.trim()) ?? allDatabases;
