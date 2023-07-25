ad350fa6f3353f256f4691fc4a49422dd7994eb3
kazaneshimizu <ks@rocketa.co.jp>
2023-07-26 06:02:20 +0900

    test: add before_login and after_login to seed data

--- test/setups/seeds/01_superfast_project_settings.ts
+++ test/setups/seeds/01_superfast_project_settings.ts
@@ -1,5 +1,11 @@
 import { Knex } from 'knex';
 
 export const seed = async (knex: Knex): Promise<void> => {
-  return await knex('superfast_project_settings').insert([{ name: 'superfast' }]);
+  return await knex('superfast_project_settings').insert([
+    {
+      name: 'superfast',
+      before_login: 'Support Hours 9:00 - 18:00',
+      after_login: '<a href="#">Contact us</a>',
+    },
+  ]);
 };
