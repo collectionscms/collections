b996dcbe865b010c4df1dfbfbdcb57e357b4aeae
kazaneshimizu <ks@rocketa.co.jp>
2023-07-26 06:03:44 +0900

    test: "it" is unified in lower case

--- test/admin/fields/schemas/authentications/login.test.ts
+++ test/admin/fields/schemas/authentications/login.test.ts
@@ -1,21 +1,21 @@
 import { loginSchema } from '../../../../../src/admin/fields/schemas/authentications/login.js';
 
 describe('Login Validations', () => {
-  it('Success', async () => {
+  it('success', async () => {
     expect(
       await loginSchema.isValid({ email: 'admin@example.com', password: '************' })
     ).toEqual(true);
   });
 
-  it('Failure (email not entered)', async () => {
+  it('failure (email not entered)', async () => {
     expect(await loginSchema.isValid({ email: '', password: '************' })).toEqual(false);
   });
 
-  it('Failure (password not entered)', async () => {
+  it('failure (password not entered)', async () => {
     expect(await loginSchema.isValid({ email: 'admin@example.com', password: '' })).toEqual(false);
   });
 
-  it('Failure (different email format)', async () => {
+  it('failure (different email format)', async () => {
     expect(
       await loginSchema.isValid({ email: 'admin/example.com', password: '************' })
     ).toEqual(false);
