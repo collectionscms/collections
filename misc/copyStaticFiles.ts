dedf775f98f0c8a19755af61570dbce808567735
kazaneshimizu <ks@rocketa.co.jp>
2023-07-26 18:52:52 +0900

    fix: copy css to dist

--- misc/copyStaticFiles.ts
+++ misc/copyStaticFiles.ts
@@ -2,4 +2,7 @@ import cpy from 'cpy';
 
 // This helper copies all static files (such as .d.ts, .json, ...etc) after compilation to dist
 await cpy(['./prepare.mjs', './package.json'], 'dist');
-await cpy(['src/**/*.d.ts', 'src/**/*.json', 'src/**/*.md', 'src/**/*.html'], 'dist/src');
+await cpy(
+  ['src/**/*.d.ts', 'src/**/*.json', 'src/**/*.md', 'src/**/*.html', 'src/**/*.css'],
+  'dist/src'
+);
