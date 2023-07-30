10412eeaad3b2edff8d5a8cf1b2a30a33088d945
kazaneshimizu <ks@rocketa.co.jp>
2023-07-30 19:55:03 +0900

    test: add create field tests

---
+++ test/setups/seeds/03_superfast_collections.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex';
+
+export const seed = async (knex: Knex): Promise<void> => {
+  await knex('superfast_collections').insert([
+    {
+      id: 1,
+      collection: 'collection_formula_one_constructors',
+      singleton: false,
+      hidden: false,
+    },
+  ]);
+
+  await knex.schema.createTable('collection_formula_one_constructors', (table) => {
+    table.increments();
+    table.timestamps(true, true);
+  });
+};
