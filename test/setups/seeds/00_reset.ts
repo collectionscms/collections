5e94bfe233bb6971efeaf5a21ac35782494d464c
kazaneshimizu <ks@rocketa.co.jp>
2023-07-30 19:55:59 +0900

    test: delete collections, fields, and drop custom tables

--- test/setups/seeds/00_reset.ts
+++ test/setups/seeds/00_reset.ts
@@ -1,7 +1,10 @@
-import { Knex } from 'knex';
+import knex, { Knex } from 'knex';
 
 export const seed = async (knex: Knex): Promise<void> => {
   await knex('superfast_project_settings').del();
   await knex('superfast_users').del();
   await knex('superfast_roles').del();
+  await knex('superfast_collections').delete();
+  await knex('superfast_fields').delete();
+  await knex.schema.dropTableIfExists('collection_formula_one_constructors');
 };
