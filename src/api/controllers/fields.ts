caa5875b860e75e205b022916df2c2e0aaad418d
kazaneshimizu <ks@rocketa.co.jp>
2023-07-30 19:32:27 +0900

    refactor: separating out logic into services, make them testable

--- src/api/controllers/fields.ts
+++ src/api/controllers/fields.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
 import { Knex } from 'knex';
 import { Field } from '../../config/types.js';
-import { InvalidPayloadException } from '../../exceptions/invalidPayload.js';
 import { SchemaOverview } from '../database/overview.js';
 import { asyncHandler } from '../middleware/asyncHandler.js';
 import {
@@ -12,6 +11,7 @@ import { BaseTransaction } from '../repositories/base.js';
 import { CollectionsRepository } from '../repositories/collections.js';
 import { FieldsRepository } from '../repositories/fields.js';
 import { RelationsRepository } from '../repositories/relations.js';
+import { FieldsService, addColumnToTable } from '../services/fields.js';
 
 const router = express.Router();
 
@@ -38,17 +38,10 @@ router.post(
   '/fields',
   permissionsHandler([{ collection: 'superfast_fields', action: 'create' }]),
   asyncHandler(async (req: Request, res: Response) => {
-    const repository = new FieldsRepository();
+    const service = new FieldsService(new FieldsRepository());
+    const field = await service.createField(req.body);
 
-    await repository.transaction(async (tx) => {
-      const fieldId = await repository.transacting(tx).create(req.body);
-      const field = await repository.transacting(tx).readOne(fieldId);
-      await tx.transaction.schema.alterTable(field.collection, (table) => {
-        addColumnToTable(field, table);
-      });
-
-      res.json({ field });
-    });
+    res.json({ field });
   })
 );
 
@@ -229,53 +222,6 @@ const deleteField = async (
     collection,
     field,
   });
-};
-
-const addColumnToTable = (field: Field, table: Knex.CreateTableBuilder, alter: boolean = false) => {
-  let column = null;
-
-  switch (field.interface) {
-    case 'input':
-      column = table.string(field.field, 255);
-      break;
-    case 'selectDropdown':
-      column = table.string(field.field, 255).defaultTo('');
-      break;
-    case 'inputMultiline':
-    case 'inputRichTextMd':
-      column = table.text(field.field);
-      break;
-    case 'boolean': {
-      const value = field.options ? JSON.parse(field.options) : null;
-      column = table.boolean(field.field).defaultTo(value?.defaultValue || false);
-      break;
-    }
-    case 'dateTime':
-      column = table.dateTime(field.field);
-      break;
-    case 'fileImage':
-      column = table
-        .integer(field.field)
-        .unsigned()
-        .index()
-        .references('id')
-        .inTable('superfast_files');
-      break;
-    case 'listOneToMany':
-      // noop
-      break;
-    case 'selectDropdownManyToOne':
-      column = table.integer(field.field).unsigned().index();
-      break;
-    default:
-      throw new InvalidPayloadException('unexpected_field_type_specified');
-  }
-
-  if (column && alter) {
-    column.alter();
-  }
-
-  return column;
 };
 
 export const fields = router;
---
+++ src/api/services/fields.ts
@@ -0,0 +1,81 @@
+import { Knex } from 'knex';
+import { Field } from '../../config/types.js';
+import { InvalidPayloadException } from '../../exceptions/invalidPayload.js';
+import { FieldsRepository } from '../repositories/fields.js';
+
+export class FieldsService {
+  repository: FieldsRepository;
+
+  constructor(repository: FieldsRepository) {
+    this.repository = repository;
+  }
+
+  /**
+   * Creates a new field in the database and adds a corresponding column to the collection's table.
+   * @param body
+   * @returns
+   */
+  async createField(body: Omit<Field, 'id'>): Promise<Field> {
+    const result = await this.repository.transaction(async (tx) => {
+      const fieldId = await this.repository.transacting(tx).create(body);
+      const field = await this.repository.transacting(tx).readOne(fieldId);
+      await tx.transaction.schema.alterTable(field.collection, (table) => {
+        addColumnToTable(field, table);
+      });
+      return field;
+    });
+
+    return result;
+  }
+}
+
+export const addColumnToTable = (
+  field: Field,
+  table: Knex.CreateTableBuilder,
+  alter: boolean = false
+) => {
+  let column = null;
+
+  switch (field.interface) {
+    case 'input':
+      column = table.string(field.field, 255);
+      break;
+    case 'selectDropdown':
+      column = table.string(field.field, 255).defaultTo('');
+      break;
+    case 'inputMultiline':
+    case 'inputRichTextMd':
+      column = table.text(field.field);
+      break;
+    case 'boolean': {
+      const value = field.options ? JSON.parse(field.options) : null;
+      column = table.boolean(field.field).defaultTo(value?.defaultValue || false);
+      break;
+    }
+    case 'dateTime':
+      column = table.dateTime(field.field);
+      break;
+    case 'fileImage':
+      column = table
+        .integer(field.field)
+        .unsigned()
+        .index()
+        .references('id')
+        .inTable('superfast_files');
+      break;
+    case 'listOneToMany':
+      // noop
+      break;
+    case 'selectDropdownManyToOne':
+      column = table.integer(field.field).unsigned().index();
+      break;
+    default:
+      throw new InvalidPayloadException('unexpected_field_type_specified');
+  }
+
+  if (column && alter) {
+    column.alter();
+  }
+
+  return column;
+};
