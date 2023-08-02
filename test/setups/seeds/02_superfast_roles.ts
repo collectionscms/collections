3eeeec41f5b005b4ad38ac16fc5a92ad87127631
kazaneshimizu <ks@rocketa.co.jp>
2023-08-03 06:07:52 +0900

    test: delete dummy data id

--- test/setups/seeds/02_superfast_roles.ts
+++ test/setups/seeds/02_superfast_roles.ts
@@ -2,6 +2,6 @@ import { Knex } from 'knex';
 
 export const seed = async (knex: Knex): Promise<void> => {
   return await knex('superfast_roles').insert([
-    { id: 1, name: 'Administrator', description: 'Administrator', admin_access: true },
+    { name: 'Administrator', description: 'Administrator', admin_access: true },
   ]);
 };
--- test/setups/seeds/03_superfast_collections.ts
+++ test/setups/seeds/03_superfast_collections.ts
@@ -3,7 +3,6 @@ import { Knex } from 'knex';
 export const seed = async (knex: Knex): Promise<void> => {
   await knex('superfast_collections').insert([
     {
-      id: 1,
       collection: 'collection_formula_one_constructors',
       singleton: false,
       hidden: false,
