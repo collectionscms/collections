6e12bc6e6dae8a2bb01cdd2b0d007c8ae09e3e6c
kazaneshimizu <ks@rocketa.co.jp>
2023-08-03 08:45:08 +0900

    fix: disable button if no create permissions

--- src/admin/pages/collections/List/Default.tsx
+++ src/admin/pages/collections/List/Default.tsx
@@ -1,12 +1,12 @@
 import { Stack, useMediaQuery, useTheme } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 import { MainCard } from 'superfast-ui';
 import { CreateNewButton } from '../../../components/elements/CreateNewButton/index.js';
 import { Link } from '../../../components/elements/Link/index.js';
 import { Cell } from '../../../components/elements/Table/Cell/index.js';
 import { Table } from '../../../components/elements/Table/index.js';
 import { Column } from '../../../components/elements/Table/types.js';
+import { useAuth } from '../../../components/utilities/Auth/index.js';
 import { ComposeWrapper } from '../../../components/utilities/ComposeWrapper/index.js';
 import { buildColumns } from '../../../utilities/buildColumns.js';
 import { ApiPreview } from '../ApiPreview/index.js';
@@ -16,7 +16,8 @@ import { Props } from './types.js';
 
 const DefaultListPageImpl: React.FC<Props> = ({ collection }) => {
   const [columns, setColumns] = useState<Column[]>([]);
-  const { t } = useTranslation();
+
+  const { hasPermission } = useAuth();
   const { getContents, getFields } = useContent();
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
@@ -43,7 +44,7 @@ const DefaultListPageImpl: React.FC<Props> = ({ collection }) => {
       title={<></>}
       secondary={
         <Stack direction={smDown ? 'column' : 'row'} alignItems="center" spacing={2}>
-          <CreateNewButton to="create" />
+          <CreateNewButton to="create" disabled={!hasPermission(collection.collection, 'create')} />
           <ApiPreview collection={collection.collection} singleton={collection.singleton} />
         </Stack>
       }
