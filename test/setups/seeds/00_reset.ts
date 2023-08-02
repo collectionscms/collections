01ddaa3fccd2556211adada45bd8bff689a5737b
kazaneshimizu <ks@rocketa.co.jp>
2023-08-02 19:32:22 +0900

    test: truncate test DB with teardown

--- test/setups/seeds/00_reset.ts
+++
@@ -1,7 +0,0 @@
-import { Knex } from 'knex';
-
-export const seed = async (knex: Knex): Promise<void> => {
-  await knex('superfast_project_settings').del();
-  await knex('superfast_users').del();
-  await knex('superfast_roles').del();
-};
