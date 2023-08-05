a7cb35867cbd1441056af7fdb2140b1b430182ec
kazaneshimizu <ks@rocketa.co.jp>
2023-08-05 14:14:32 +0900

    refactor: separate logic from controller to service

--- src/api/controllers/collections.ts
+++ src/api/controllers/collections.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import { Field } from '../../config/types.js';
 import { RecordNotFoundException } from '../../exceptions/database/recordNotFound.js';
 import { SchemaOverview } from '../database/overview.js';
 import { asyncHandler } from '../middleware/asyncHandler.js';
@@ -9,6 +8,7 @@ import { CollectionsRepository } from '../repositories/collections.js';
 import { FieldsRepository } from '../repositories/fields.js';
 import { PermissionsRepository } from '../repositories/permissions.js';
 import { RelationsRepository } from '../repositories/relations.js';
+import { CollectionsService } from '../services/collections.js';
 
 const router = express.Router();
 
@@ -47,64 +47,10 @@ router.post(
   '/collections',
   permissionsHandler([{ collection: 'superfast_collections', action: 'create' }]),
   asyncHandler(async (req: Request, res: Response) => {
-    const repository = new CollectionsRepository();
-    const fieldsRepository = new FieldsRepository();
+    const service = new CollectionsService(new CollectionsRepository(), new FieldsRepository());
+    const collectionId = await service.createCollection(req.body);
 
-    const data = req.body.status
-      ? {
-          ...req.body,
-          status_field: 'status',
-          draft_value: 'draft',
-          publish_value: 'published',
-          archive_value: 'archived',
-        }
-      : req.body;
-    delete data.status;
-
-    await repository.transaction(async (tx) => {
-      const collectionId = await repository.transacting(tx).create(data);
-
-      await tx.transaction.schema.createTable(req.body.collection, (table) => {
-        table.increments();
-        req.body.status && table.string('status').notNullable();
-      });
-
-      const fields: Field[] = [
-        {
-          collection: req.body.collection,
-          field: 'id',
-          label: 'id',
-          interface: 'input',
-          required: true,
-          readonly: true,
-          hidden: true,
-          special: null,
-          sort: null,
-          options: null,
-        },
-        req.body.status && {
-          collection: req.body.collection,
-          field: 'status',
-          label: 'Status',
-          interface: 'selectDropdownStatus',
-          required: true,
-          readonly: false,
-          hidden: false,
-          special: null,
-          sort: null,
-          options: JSON.stringify({
-            choices: [
-              { label: 'Draft', value: 'draft' },
-              { label: 'Published', value: 'published' },
-              { label: 'Archived', value: 'archived' },
-            ],
-          }),
-        },
-      ];
-      await fieldsRepository.transacting(tx).createMany(fields);
-
-      res.json({ id: collectionId });
-    });
+    res.json({ id: collectionId });
   })
 );
 
