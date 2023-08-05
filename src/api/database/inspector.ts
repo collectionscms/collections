90302a1d4cb383da76df12c7236812722c1b7f73
kazaneshimizu <ks@rocketa.co.jp>
2023-08-06 08:07:27 +0900

    refactor: make DB connections injectable

--- src/api/database/inspector.ts
+++ src/api/database/inspector.ts
@@ -1,7 +1,7 @@
+import { Knex } from 'knex';
 import { SchemaInspector } from 'knex-schema-inspector';
-import { getDatabase } from './connection.js';
 
-type SchemaInfo = {
+export type SchemaInfo = {
   [table: string]: {
     table: string;
     columns: {
@@ -22,8 +22,8 @@ type ColumnInfo = {
   max_length: number | null;
 };
 
-export const getSchemaInfo = async (): Promise<SchemaInfo> => {
-  const inspector = SchemaInspector(getDatabase());
+export const getSchemaInfo = async (database: Knex): Promise<SchemaInfo> => {
+  const inspector = SchemaInspector(database);
 
   const result: SchemaInfo = {};
 
@@ -31,20 +31,23 @@ export const getSchemaInfo = async (): Promise<SchemaInfo> => {
   for (let table of tables) {
     const columns = await inspector.columnInfo(table);
 
-    const tableColumns = columns.reduce((acc, column) => {
-      acc[column.name] = {
-        table_name: column.table,
-        column_name: column.name,
-        default_value: column.default_value,
-        is_nullable: column.is_nullable,
-        is_generated: column.is_generated,
-        data_type: column.data_type,
-        numeric_precision: column.numeric_precision,
-        numeric_scale: column.numeric_scale,
-        max_length: column.max_length,
-      };
-      return acc;
-    }, {} as { [name: string]: ColumnInfo });
+    const tableColumns = columns.reduce(
+      (acc, column) => {
+        acc[column.name] = {
+          table_name: column.table,
+          column_name: column.name,
+          default_value: column.default_value,
+          is_nullable: column.is_nullable,
+          is_generated: column.is_generated,
+          data_type: column.data_type,
+          numeric_precision: column.numeric_precision,
+          numeric_scale: column.numeric_scale,
+          max_length: column.max_length,
+        };
+        return acc;
+      },
+      {} as { [name: string]: ColumnInfo }
+    );
 
     result[table] = {
       table: table,
