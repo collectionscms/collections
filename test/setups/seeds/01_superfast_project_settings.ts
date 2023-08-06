037691be995ea840c557aa51f43ecbccd22c7863
kazaneshimizu <ks@rocketa.co.jp>
2023-08-06 11:23:49 +0900

    test: rename property

--- test/setups/seeds/01_superfast_project_settings.ts
+++ test/setups/seeds/01_superfast_project_settings.ts
@@ -1,7 +1,7 @@
 import { Knex } from 'knex';
 
-export const seed = async (knex: Knex): Promise<void> => {
-  return await knex('superfast_project_settings').insert([
+export const seed = async (database: Knex): Promise<void> => {
+  return await database('superfast_project_settings').insert([
     {
       name: 'superfast',
       before_login: 'Support Hours 9:00 - 18:00',
