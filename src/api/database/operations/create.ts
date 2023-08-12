de80edbc8441e90e296e94522d8b7814502c6cd0
kazaneshimizu <ks@rocketa.co.jp>
2023-08-13 06:27:09 +0900

    refactor: create database insert method

---
+++ src/api/database/operations/create.ts
@@ -0,0 +1,26 @@
+import { Knex } from 'knex';
+import { PrimaryKey } from '../schemas.js';
+
+export type Arguments = {
+  collection: string;
+  database: Knex;
+  data: Record<string, unknown>;
+};
+
+export const create = async (args: Arguments): Promise<PrimaryKey> => {
+  let { database, collection, data } = args;
+
+  const builder = database(collection).insert(data);
+
+  switch (database.client.config.client) {
+    case 'sqlite3':
+    case 'pg':
+      builder.returning('id');
+      break;
+  }
+
+  const result = await builder.then((result) => result[0]);
+  const key = typeof result === 'object' ? result['id'] : result;
+
+  return key;
+};
