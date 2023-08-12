1560aa907eeac9d694bc85b024a14b6d36813e00
kazaneshimizu <ks@rocketa.co.jp>
2023-08-13 06:27:24 +0900

    refactor: create database update method

---
+++ src/api/database/operations/update.ts
@@ -0,0 +1,22 @@
+import { Knex } from 'knex';
+import { getSchemaInfo } from '../inspector.js';
+import { PrimaryKey } from '../schemas.js';
+
+export type Arguments = {
+  collection: string;
+  database: Knex;
+  key: PrimaryKey;
+  data: Record<string, unknown>;
+};
+
+export const update = async (args: Arguments): Promise<PrimaryKey> => {
+  let { database, collection, key, data } = args;
+
+  const schemaInfo = await getSchemaInfo(database);
+
+  if (schemaInfo[args.collection].columns.updated_at) {
+    data = { ...data, updated_at: database.fn.now() };
+  }
+
+  return database(collection).where({ id: key }).update(data);
+};
