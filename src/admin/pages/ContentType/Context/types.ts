d351ec93f9b723d30999e1b377ca7cd95ba9243a
kazaneshimizu <ks@rocketa.co.jp>
2023-08-19 13:53:00 +0900

    refactor: make collection creation type-safe

--- src/admin/pages/ContentType/Context/types.ts
+++ src/admin/pages/ContentType/Context/types.ts
@@ -1,11 +1,11 @@
 import { SWRConfiguration, SWRResponse } from 'swr';
 import { SWRMutationResponse } from 'swr/mutation';
-import { Collection, GetCollection, GetField } from '../../../../config/types.js';
+import { Collection, GetCollection, GetField, PostCollection } from '../../../../config/types.js';
 
 export type CollectionContext = {
   getCollection: (id: string) => SWRMutationResponse<GetCollection>;
   getCollections: () => SWRResponse<Collection[]>;
-  createCollection: SWRMutationResponse<number, any, string, Record<string, any>>;
+  createCollection: SWRMutationResponse<number, any, string, Omit<PostCollection, 'id'>>;
   updateCollection: (id: string) => SWRMutationResponse<void, any, string, Record<string, any>>;
   getFields: (collection: string | null, config?: SWRConfiguration) => SWRResponse<GetField[]>;
   updateFields: () => SWRMutationResponse<void, any, string, Record<string, any>>;
