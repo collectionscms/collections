83e7c6106947d5be6dcad156616ba3accdeb48cb
kazaneshimizu <ks@rocketa.co.jp>
2023-08-21 13:15:48 +0900

    test: add converted timestamp

--- test/setups/seeds/01_superfast_project_settings.ts
+++ test/setups/seeds/01_superfast_project_settings.ts
@@ -1,11 +1,16 @@
 import { Knex } from 'knex';
+import { getHelpers } from '../../../src/api/database/helpers/index.js';
 
 export const seed = async (database: Knex): Promise<void> => {
+  const helpers = getHelpers(database);
+
   return await database('superfast_project_settings').insert([
     {
       name: 'superfast',
       before_login: 'Support Hours 9:00 - 18:00',
       after_login: '<a href="#">Contact us</a>',
+      created_at: helpers.date.writeTimestamp(new Date().toISOString()),
+      updated_at: helpers.date.writeTimestamp(new Date().toISOString()),
     },
   ]);
 };
