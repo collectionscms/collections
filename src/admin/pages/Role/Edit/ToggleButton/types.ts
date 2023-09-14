b63d25540d4625d706c69ef610ca7490b3bab019
kazaneshimizu <ks@rocketa.co.jp>
2023-09-14 09:40:53 +0900

    refactor: add collection id to post data for permission registration

--- src/admin/pages/Role/Edit/ToggleButton/types.ts
+++ src/admin/pages/Role/Edit/ToggleButton/types.ts
@@ -1,9 +1,9 @@
-import { Permission, PermissionsAction } from '../../../../config/types.js';
+import { Collection, Permission, PermissionsAction } from '../../../../config/types.js';
 
 export type Props = {
   roleId: string;
   permissions: Permission[];
-  collection: string;
+  collection: Collection;
   action: PermissionsAction;
   onSuccess: (permissionId: number) => void;
 };
