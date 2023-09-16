4e38622140b0a8463f3fb9b690e99a30720b93f3
kazaneshimizu <ks@rocketa.co.jp>
2023-09-16 09:41:15 +0900

    feat: add validation for profile updates

---
+++ src/admin/fields/schemas/profile/updateUser.ts
@@ -0,0 +1,32 @@
+import { TFunction } from 'i18next';
+import { ObjectSchema } from 'yup';
+import { yup } from '../../yup.js';
+
+export type FormValues = {
+  name: string;
+  email: string;
+  password?: string | null;
+  api_key?: string | null;
+};
+
+export const updateUser = (t: TFunction): ObjectSchema<FormValues> => {
+  return yup.object().shape({
+    name: yup.string().required().max(250),
+    email: yup.string().required().email().max(250),
+    password: yup
+      .string()
+      .notRequired()
+      .matches(/[a-zA-z0-9@$!%*#?&]+/, {
+        message: t('yup.custom.alphanumeric_and_special_character'),
+        excludeEmptyString: true,
+      })
+      .min(4)
+      .max(250)
+      .transform((value) => value || null),
+    api_key: yup
+      .string()
+      .notRequired()
+      .max(250)
+      .transform((value) => value || null),
+  });
+};
