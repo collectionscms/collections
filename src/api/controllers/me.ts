1c1fa48570ab480f4137e9cc08cbc66994040e92
kazaneshimizu <ks@rocketa.co.jp>
2023-09-16 12:33:03 +0900

    feat: implement profile updates

--- src/api/controllers/me.ts
+++ src/api/controllers/me.ts
@@ -2,9 +2,11 @@ import express, { Request, Response } from 'express';
 import { cookieOptions } from '../../constants.js';
 import { env } from '../../env.js';
 import { RecordNotFoundException } from '../../exceptions/database/recordNotFound.js';
+import { InvalidCredentialsException } from '../../exceptions/invalidCredentials.js';
 import { asyncHandler } from '../middleware/asyncHandler.js';
 import { UsersService } from '../services/users.js';
 import { getExtractJwt } from '../utilities/getExtractJwt.js';
+import { oneWayHash } from '../utilities/oneWayHash.js';
 import { sign } from '../utilities/sign.js';
 import { verifyJwt } from '../utilities/verifyJwt.js';
 
@@ -50,6 +52,26 @@ router.get(
     }
 
     res.json({ user: null, apiKey: null, token: null });
+  })
+);
+
+router.patch(
+  '/me',
+  asyncHandler(async (req: Request, res: Response) => {
+    if (!req.userId) {
+      throw new InvalidCredentialsException('invalid_user_credentials');
+    }
+
+    const service = new UsersService({ schema: req.schema });
+    await service.checkUniqueEmail(req.body.email, req.userId);
+
+    const data = req.body.password
+      ? { ...req.body, password: await oneWayHash(req.body.password) }
+      : req.body;
+
+    await service.updateOne(req.userId, data);
+
+    res.status(204).end();
   })
 );
 
