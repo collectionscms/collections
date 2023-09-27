b03cc37795eb4b396ce7d4a2b2a47c009f330c90
kazaneshimizu <ks@rocketa.co.jp>
2023-09-28 07:17:30 +0900

    fix: include README, LICENSE in npm package

--- misc/copyStaticFiles.ts
+++ misc/copyStaticFiles.ts
@@ -1,7 +1,7 @@
 import cpy from 'cpy';
 
 // This helper copies all static files (such as .d.ts, .json, ...etc) after compilation to dist
-await cpy(['./prepare.mjs', './package.json'], 'dist');
+await cpy(['./prepare.mjs', './package.json', './README.md', './LICENSE'], 'dist');
 await cpy(
   ['src/**/*.d.ts', 'src/**/*.json', 'src/**/*.md', 'src/**/*.html', 'src/**/*.css'],
   'dist/src'
