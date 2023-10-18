f4a0ab44fcb932336a4b2d00bebd1eb03dca584f
kazaneshimizu <ks@rocketa.co.jp>
2023-10-18 09:42:46 +0900

    style: add meta information

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,10 +1,14 @@
-import { DocsThemeConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
 import React from 'react';
 
+const defaultTitle = 'Collections';
+
 const config: DocsThemeConfig = {
-  logo: <span>Collections</span>,
+  logo: <span>{defaultTitle}</span>,
   head: (
     <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link
         href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700;900&display=swap"
         rel="stylesheet"
@@ -12,11 +16,48 @@ const config: DocsThemeConfig = {
     </>
   ),
   useNextSeoProps: function SEO() {
-    const defaultTitle = 'Collections';
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+    const titleTemplate = asPath !== '/' ? `%s – ${defaultTitle}` : defaultTitle;
+    const defaultDescription =
+      'Collections is an open-source headless CMS that turns WordPress posts into APIs with drag and drop.';
 
     return {
-      defaultTitle,
-      titleTemplate: `%s – ${defaultTitle}`,
+      defaultTitle: frontMatter.title || titleTemplate,
+      description: frontMatter.description || defaultDescription,
+      titleTemplate,
+      openGraph: {
+        type: 'website',
+        title: titleTemplate,
+        description: frontMatter.description || defaultDescription,
+        url: 'https://collections.dev',
+        siteName: defaultTitle,
+        images: [
+          {
+            url: 'https://cdn.collections.dev/docs/og-image.png',
+            width: 1200,
+            height: 630,
+            alt: 'collections og image',
+            type: 'png',
+          },
+        ],
+      },
+      twitter: {
+        handle: '@collectionscms',
+        site: '@collectionscms',
+        cardType: 'summary_large_image',
+      },
+      additionalLinkTags: [
+        {
+          rel: 'icon',
+          href: '/images/favicon.svg',
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/images/apple-icon.png',
+          sizes: '180x180',
+        },
+      ],
     };
   },
   project: {
