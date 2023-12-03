0b2c6b288e31ce9098918278f1bd11bbaa610524
kazaneshimizu <ks@rocketa.co.jp>
2023-12-03 17:18:59 +0900

    test: add test

---
+++ test/api/middleware/modelExists.test.ts
@@ -0,0 +1,68 @@
+import { jest } from '@jest/globals';
+import { NextFunction, Request, Response, response } from 'express';
+import { modelExists } from '../../../src/api/middleware/modelExists.js';
+
+describe('ModelExists middleware', () => {
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+  let nextFunction: NextFunction = jest.fn();
+
+  describe('modelExists', () => {
+    beforeEach(() => {
+      mockRequest = {
+        schema: {
+          models: {
+            1: {
+              id: 1,
+              model: 'mock_model',
+              singleton: false,
+              statusField: null,
+              draftValue: null,
+              publishValue: null,
+              archiveValue: null,
+              fields: {
+                id: { alias: false, special: null, field: 'id' },
+                createdAt: { alias: false, special: null, field: 'createdAt' },
+                updatedAt: { alias: false, special: null, field: 'updatedAt' },
+                date: { alias: false, special: 'cast-timestamp', field: 'date' },
+              },
+            },
+          },
+          relations: [],
+        },
+      };
+    });
+
+    it('should get model by id', () => {
+      mockRequest.params = {
+        identifier: '1',
+      };
+
+      modelExists(mockRequest as Request, mockResponse as Response, nextFunction);
+      expect(mockRequest.model?.model).toBe('mock_model');
+    });
+
+    it('should get model by name', () => {
+      mockRequest.params = {
+        identifier: 'mock_model',
+      };
+
+      modelExists(mockRequest as Request, mockResponse as Response, nextFunction);
+      expect(mockRequest.model?.id).toBe(1);
+    });
+
+    it('should call next function', () => {
+      modelExists(mockRequest as Request, mockResponse as Response, nextFunction);
+      expect(nextFunction).toHaveBeenCalled();
+    });
+
+    it('should throw forbidden', async () => {
+      mockRequest.params = {
+        identifier: 'no_exist_model',
+      };
+
+      modelExists(mockRequest as Request, mockResponse as Response, nextFunction);
+      expect(mockRequest.model).toBeUndefined();
+    });
+  });
+});
