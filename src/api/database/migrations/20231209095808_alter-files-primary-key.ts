6bffa8410bfc57acb02f56cef8f9122abe3a291d
kazaneshimizu <ks@rocketa.co.jp>
2023-12-12 23:09:08 +0900

    fix: change files primary key integer to uuid

--- src/api/database/migrations/20231209095808_alter-files-primary-key.ts
+++ src/api/database/migrations/20231209095808_alter-files-primary-key.ts
@@ -1,4 +1,5 @@
 import { Knex } from 'knex';
+import { v4 as uuidv4 } from 'uuid';
 
 const getDistinctFileImageModels = async (knex: Knex) => {
   return await knex
@@ -8,36 +9,50 @@ const getDistinctFileImageModels = async (knex: Knex) => {
     .where('interface', '=', 'fileImage');
 };
 
-const changePrimaryKey = async (knex: Knex) => {
+const changePrimaryKey = async (knex: Knex, field?: any) => {
   await knex.schema.table('CollectionsFiles', (table) => {
-    table.uuid('uuid').notNullable();
+    table.uuid('uuid');
   });
-  await knex('CollectionsFiles').update({ uuid: knex.raw('id') });
 
-  await knex.schema.alterTable('CollectionsFiles', (table) => {
+  if (field) {
+    const rows = await knex.select('*').from('CollectionsFiles');
+    for (const row of rows) {
+      const uuid = uuidv4();
+      await knex('CollectionsFiles').update({ uuid: uuid }).where('id', '=', row.id);
+      await knex(field.model)
+        .update({ [`${field.field}_uuid`]: uuid })
+        .where('id', '=', row.id);
+    }
+  }
+
+  await knex.schema.table('CollectionsFiles', (table) => {
     table.dropColumn('id');
     table.renameColumn('uuid', 'id');
+    table.setNullable('id');
+    table.primary(['id']);
   });
 };
 
 export async function up(knex: Knex): Promise<void> {
   const fields = await getDistinctFileImageModels(knex);
   if (!fields.length) {
-    return await changePrimaryKey(knex);
+    return await changePrimaryKey(knex, []);
   }
 
   for (const field of fields) {
-    // drop foreign key
-    await knex.schema.alterTable(field.model, (table) => {
+    // drop foreign key and add escape field
+    await knex.schema.table(field.model, (table) => {
       table.dropForeign([field.field]);
-      table.uuid(field.field).alter();
+      table.uuid(`${field.field}_uuid`);
     });
 
-    // change primary key data type (integer to string)
-    await changePrimaryKey(knex);
+    // change primary key data type (integer to uuid)
+    await changePrimaryKey(knex, field);
 
-    // attach foreign key
-    await knex.schema.alterTable(field.model, (table) => {
+    // attach foreign key and drop escape field
+    await knex.schema.table(field.model, (table) => {
+      table.dropColumn(field.field);
+      table.renameColumn(`${field.field}_uuid`, field.field);
       table.foreign(field.field).references('CollectionsFiles.id');
     });
   }
