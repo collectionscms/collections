fd621b3d8c1fd8be5696701c9757395685d6341a
kazaneshimizu <ks@rocketa.co.jp>
2024-01-13 17:42:27 +0900

    refactor: remove permission crud

--- src/api/controllers/roles.ts
+++ src/api/controllers/roles.ts
@@ -33,10 +33,10 @@ router.post(
   '/roles',
   asyncHandler(async (req: Request, res: Response) => {
     const service = new RolesService(prisma);
-    const roleId = await service.create(req.body);
+    const role = await service.create(req.body);
 
     res.json({
-      id: roleId,
+      id: role.id,
     });
   })
 );
@@ -68,38 +68,6 @@ router.get(
     const permissions = await service.findRolePermissions(req.params.id);
 
     res.json({ permissions });
-  })
-);
-
-router.post(
-  '/roles/:id/permissions',
-  asyncHandler(async (req: Request, res: Response) => {
-    // const id = Number(req.params.id);
-    // const data = {
-    //   ...req.body,
-    //   roleId: id,
-    // };
-
-    // const service = new PermissionsService(prisma);
-    // const permissionId = await permissionsService.createOne(data);
-
-    // res.json({
-    //   id: permissionId,
-    // });
-
-    res.status(204).end();
-  })
-);
-
-router.delete(
-  '/roles/:id/permissions/:permissionId',
-  asyncHandler(async (req: Request, res: Response) => {
-    // const permissionId = Number(req.params.permissionId);
-
-    // const service = new PermissionsService(prisma);
-    // await permissionsService.deleteOne(permissionId);
-
-    res.status(204).end();
   })
 );
 
