9cf6c70710d8d0c20c972d1ada1ed274c95f9139
kazaneshimizu <ks@rocketa.co.jp>
2024-01-22 09:57:33 +0900

    feat: replace express auth by auth.js

---
+++ src/api/configs/auth.ts
@@ -0,0 +1,47 @@
+import CredentialsProvider from '@auth/express/providers/credentials';
+import { logger } from '../../utilities/logger.js';
+import { prisma } from '../database/prisma/client.js';
+import { UsersService } from '../services/users.js';
+
+export const authConfig = {
+  callbacks: {
+    async jwt(params: any) {
+      const { token, user } = params;
+      if (user) {
+        token.userId = user.id;
+      }
+
+      return token;
+    },
+    async session(params: any) {
+      const { session, token } = params;
+      if (token.user) {
+        session.user = token.user;
+      }
+
+      return session;
+    },
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+
+        try {
+          const service = new UsersService(prisma);
+          const user = await service.login(String(email), String(password));
+          return user;
+        } catch (e) {
+          logger.error(e);
+        }
+
+        return null;
+      },
+    }),
+  ],
+};
--- src/api/router/apiRouter.ts
+++ src/api/router/apiRouter.ts
@@ -1,14 +1,16 @@
+import { ExpressAuth } from '@auth/express';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import { env } from '../../env.js';
 import { expressLogger } from '../../utilities/expressLogger.js';
+import { authConfig } from '../configs/auth.js';
 import { authentications } from '../controllers/authentications.js';
 import { files } from '../controllers/files.js';
 import { me } from '../controllers/me.js';
 import { project } from '../controllers/projects.js';
 import { roles } from '../controllers/roles.js';
 import { users } from '../controllers/users.js';
-import { authHandler } from '../middleware/authHandler.js';
+import { currentSession } from '../middleware/auth.js';
 import { corsMiddleware } from '../middleware/cors.js';
 import { extractTokenHandler } from '../middleware/extractTokenHandler.js';
 
@@ -22,9 +24,9 @@ router.use(cookieParser());
 router.use(express.json({ limit: env.REQ_LIMIT }));
 router.use(express.urlencoded({ limit: env.REQ_LIMIT, extended: true }));
 router.use(expressLogger);
-
+router.use(currentSession);
+router.use('/auth/*', ExpressAuth(authConfig));
 router.use(extractTokenHandler);
-router.use(authHandler);
 
 router.get('/health', (_req, res) => res.send('ok'));
 
