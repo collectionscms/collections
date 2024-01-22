80d658e2758a18d3869237a4a9ff4d58bd6572ad
kazaneshimizu <ks@rocketa.co.jp>
2024-01-22 22:13:41 +0900

    chore: delete jwt

--- src/api/utilities/sign.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { AuthUser } from '../../configs/types.js';
-import { env } from '../../env.js';
-
-export const sign = (user: AuthUser, expiresIn: number): string => {
-  return jwt.sign(user, env.SECRET, {
-    expiresIn,
-  });
-};
