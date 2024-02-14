54b40d6671fe4fe4ac05c2921fdebe4828a01b87
kazaneshimizu <ks@rocketa.co.jp>
2024-02-14 21:58:54 +0900

    feat: add edit post route

---
+++ src/admin/components/Routes/Post/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import lazy from '../../../utilities/lazy.js';
+import { Loader } from '../../elements/Loader/index.js';
+import { EditorLayout } from '../../layouts/Editor/index.js';
+
+const EditPost = Loader(lazy(() => import('../../../pages/Post/Edit/index.js'), 'EditPostPage'));
+
+export const PostRoutes = () => {
+  return {
+    path: '/admin/posts/:slug',
+    element: <EditorLayout />,
+    children: [
+      {
+        path: '',
+        element: <EditPost />,
+      },
+    ],
+  };
+};
---
+++ src/admin/components/layouts/Editor/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+export const EditorLayout: React.FC = () => {
+  return <Outlet />;
+};
