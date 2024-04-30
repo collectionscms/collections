dc90a5134f44affb8189850d12d285b68a64498d
kazaneshimizu <ks@rocketa.co.jp>
2024-05-01 07:41:10 +0900

    refactor: logout

--- src/admin/pages/Logout/index.tsx
+++ src/admin/pages/Logout/index.tsx
@@ -1,13 +1,10 @@
-import { Box, Button, Typography } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2/Grid2.js';
 import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Link as RouterLink } from 'react-router-dom';
 import { logger } from '../../../utilities/logger.js';
+import { Loading } from '../../components/elements/Loading/index.js';
 import { useAuth } from '../../components/utilities/Auth/index.js';
+import { getLoginUrl } from '../../utilities/urlGenerator.js';
 
 export const Logout: React.FC = () => {
-  const { t } = useTranslation();
   const { getCsrfToken, logout } = useAuth();
   const { data: csrfToken } = getCsrfToken();
   const { trigger } = logout();
@@ -29,27 +26,5 @@ export const Logout: React.FC = () => {
     }
   }, [csrfToken]);
 
-  return (
-    <Grid container spacing={3}>
-      <Grid xs={12}>
-        <Box sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
-          <Typography variant="h3">{t('logged_out')}</Typography>
-        </Box>
-      </Grid>
-      <Grid xs={12}>
-        <Button
-          component={RouterLink}
-          to="/admin/auth/login"
-          disableElevation
-          fullWidth
-          size="large"
-          type="submit"
-          variant="contained"
-          color="primary"
-        >
-          {t('log_back_in')}
-        </Button>
-      </Grid>
-    </Grid>
-  );
+  return <Loading />;
 };
