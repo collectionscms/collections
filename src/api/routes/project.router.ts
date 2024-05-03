e2d7d539b0572cf67fac5fc219c50c7955a23997
kazaneshimizu <ks@rocketa.co.jp>
2024-05-03 16:27:18 +0900

    refactor: validate project id

--- src/api/routes/project.router.ts
+++ src/api/routes/project.router.ts
@@ -4,6 +4,8 @@ import { ProjectRepository } from '../data/project/project.repository.js';
 import { projectPrisma } from '../database/prisma/client.js';
 import { asyncHandler } from '../middlewares/asyncHandler.js';
 import { authenticatedUser } from '../middlewares/auth.js';
+import { getProjectUseCaseSchema } from '../useCases/project/getProject.schema.js';
+import { GetProjectUseCase } from '../useCases/project/getProject.useCase.js';
 import { updateProjectUseCaseSchema } from '../useCases/project/updateProject.schema.js';
 import { UpdateProjectUseCase } from '../useCases/project/updateProject.useCase.js';
 
@@ -13,12 +15,18 @@ router.get(
   '/projects',
   authenticatedUser,
   asyncHandler(async (_req: Request, res: Response) => {
-    const id = res.user.projects[0].id;
+    const validated = getProjectUseCaseSchema.safeParse({
+      projectId: res.tenantProjectId,
+    });
+    if (!validated.success) throw new InvalidPayloadException('bad_request', validated.error);
 
-    const repository = new ProjectRepository();
-    const result = await repository.findOneById(projectPrisma(id), id);
+    const useCase = new GetProjectUseCase(
+      projectPrisma(validated.data.projectId),
+      new ProjectRepository()
+    );
+    const project = await useCase.execute(validated.data.projectId);
 
-    res.json({ project: result.toResponse() });
+    res.json({ project });
   })
 );
 
@@ -26,15 +34,16 @@ router.patch(
   '/projects',
   authenticatedUser,
   asyncHandler(async (req: Request, res: Response) => {
-    const id = res.user.projects[0].id;
-
     const validated = updateProjectUseCaseSchema.safeParse({
-      id,
+      id: res.tenantProjectId,
       ...req.body,
     });
     if (!validated.success) throw new InvalidPayloadException('bad_request', validated.error);
 
-    const projectUseCase = new UpdateProjectUseCase(projectPrisma(id), new ProjectRepository());
+    const projectUseCase = new UpdateProjectUseCase(
+      projectPrisma(validated.data.id),
+      new ProjectRepository()
+    );
     await projectUseCase.execute(validated.data.id, {
       name: validated.data.name,
     });
