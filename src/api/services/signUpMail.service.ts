fbb745d400f12ca9d9bab0108603b1a50d3246b3
kazaneshimizu <ks@rocketa.co.jp>
2024-05-21 05:47:49 +0900

    feat: implement verify mail

---
+++ src/api/services/signUpMail.service.ts
@@ -0,0 +1,18 @@
+import { env } from '../../env.js';
+import { UserEntity } from '../data/user/user.entity.js';
+import { MailService } from './mail.service.js';
+
+export class SignUpMailService extends MailService {
+  async sendVerify(entity: UserEntity): Promise<void> {
+    const url = `${env.PUBLIC_SERVER_URL}/admin/auth/verify?token=${entity.confirmationToken}`;
+    const html = `Hi! Thank you for creating a Collection account.
+    Please validate your email address. Once verified, you will be able to log in successfully.<br/><br/>
+    <a href="${url}">${url}</a><br/><br/>`;
+
+    super.sendEmail('Collections', {
+      to: entity.email,
+      subject: 'Complete your account',
+      html,
+    });
+  }
+}
