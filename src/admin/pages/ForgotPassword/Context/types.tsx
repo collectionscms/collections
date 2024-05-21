240cdb84d140591f8918c45b817c98ba692078dd
kazaneshimizu <ks@rocketa.co.jp>
2024-05-21 19:43:22 +0900

    refactor: return value

--- src/admin/pages/ForgotPassword/Context/types.tsx
+++
@@ -1,5 +0,0 @@
-import { SWRMutationResponse } from 'swr/mutation';
-
-export type ForgotPasswordContext = {
-  forgotPassword: () => SWRMutationResponse<string, any, string, Record<string, any>>;
-};
