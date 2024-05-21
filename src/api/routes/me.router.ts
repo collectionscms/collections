d2e0d7d85e389577ddbd96957f9db7db7365f49b
kazaneshimizu <ks@rocketa.co.jp>
2024-05-21 19:42:00 +0900

    refactor: forgot password to useCase

--- src/api/routes/me.router.ts
+++ src/api/routes/me.router.ts
@@ -1,20 +1,20 @@
 import { getSession } from '@auth/express';
 import express, { Request, Response } from 'express';
-import { env } from '../../env.js';
-import { InvalidCredentialsException } from '../../exceptions/invalidCredentials.js';
 import { InvalidPayloadException } from '../../exceptions/invalidPayload.js';
 import { authConfig } from '../configs/auth.js';
 import { UserRepository } from '../data/user/user.repository.js';
 import { bypassPrisma, prisma } from '../database/prisma/client.js';
 import { asyncHandler } from '../middlewares/asyncHandler.js';
 import { authenticatedUser } from '../middlewares/auth.js';
-import { MailService } from '../services/mail.service.js';
+import { ResetPasswordMailService } from '../services/resetPasswordMail.service.js';
 import { getMyProfileUseCaseSchema } from '../useCases/me/getMyProfile.schema.js';
 import { GetMyProfileUseCase } from '../useCases/me/getMyProfile.useCase.js';
 import { getMyProjectsUseCaseSchema } from '../useCases/me/getMyProjects.schema.js';
 import { GetMyProjectsUseCase } from '../useCases/me/getMyProjects.useCase.js';
 import { updateProfileUseCaseSchema } from '../useCases/me/updateProfile.schema.js';
 import { UpdateProfileUseCase } from '../useCases/me/updateProfile.useCase.js';
+import { forgotPasswordUseCaseSchema } from '../useCases/user/forgotPassword.schema.js';
+import { ForgotPasswordUseCase } from '../useCases/user/forgotPassword.useCase.js';
 import { resetPasswordUseCaseSchema } from '../useCases/user/resetPassword.schema.js';
 import { ResetPasswordUseCase } from '../useCases/user/resetPassword.useCase.js';
 
@@ -82,7 +82,6 @@ router.patch(
 
 router.post(
   '/me/reset-password',
-  authenticatedUser,
   asyncHandler(async (req: Request, res: Response) => {
     const validated = resetPasswordUseCaseSchema.safeParse({
       token: req.body.token,
@@ -99,35 +98,20 @@ router.post(
 
 router.post(
   '/me/forgot-password',
-  authenticatedUser,
   asyncHandler(async (req: Request, res: Response) => {
-    const repository = new UserRepository();
-    const user = await repository.findOneByEmail(prisma, req.body.email);
-
-    if (!user) {
-      throw new InvalidCredentialsException('unregistered_email_address');
-    }
-
-    user.resetPassword();
-    const token = await repository.updateResetPasswordToken(prisma, user);
-
-    const html = `You are receiving this message because you have requested a password reset for your account.<br/>
-    Please click the following link and enter your new password.<br/><br/>
-    <a href="${env.PUBLIC_SERVER_URL}/admin/auth/reset-password/${token}">
-      ${env.PUBLIC_SERVER_URL}/admin/auth/reset-password/${token}
-    </a><br/><br/>
-    If you did not request this, please ignore this email and your password will remain unchanged.`;
-
-    const mail = new MailService();
-    mail.sendEmail('Collections', {
-      to: req.body.email,
-      subject: 'Password Reset Request',
-      html,
+    const validated = forgotPasswordUseCaseSchema.safeParse({
+      email: req.body.email,
     });
+    if (!validated.success) throw new InvalidPayloadException('bad_request', validated.error);
 
-    res.json({
-      message: 'success',
-    });
+    const useCase = new ForgotPasswordUseCase(
+      prisma,
+      new UserRepository(),
+      new ResetPasswordMailService()
+    );
+    await useCase.execute(validated.data);
+
+    res.status(204).end();
   })
 );
 
---
+++ src/api/useCases/user/forgotPassword.useCase.ts
@@ -0,0 +1,33 @@
+import { User } from '@prisma/client';
+import { InvalidCredentialsException } from '../../../exceptions/invalidCredentials.js';
+import { UnexpectedException } from '../../../exceptions/unexpected.js';
+import { UserRepository } from '../../data/user/user.repository.js';
+import { PrismaType } from '../../database/prisma/client.js';
+import { ResetPasswordMailService } from '../../services/resetPasswordMail.service.js';
+import { ForgotPasswordUseCaseSchemaType } from './forgotPassword.schema.js';
+
+export class ForgotPasswordUseCase {
+  constructor(
+    private readonly prisma: PrismaType,
+    private readonly userRepository: UserRepository,
+    private readonly mailService: ResetPasswordMailService
+  ) {}
+
+  async execute(props: ForgotPasswordUseCaseSchemaType): Promise<User> {
+    const user = await this.userRepository.findOneByEmail(this.prisma, props.email);
+    if (!user) {
+      throw new InvalidCredentialsException('unregistered_email_address');
+    }
+
+    user.resetPassword();
+
+    const result = await this.userRepository.updateResetPasswordToken(this.prisma, user);
+    if (!result.resetPasswordToken) {
+      throw new UnexpectedException({ message: 'reset password token is required' });
+    }
+
+    this.mailService.sendResetPassword(result.resetPasswordToken, result.email);
+
+    return result.toResponse();
+  }
+}
