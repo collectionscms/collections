bcf09e13ab959f85aa7784a11f7c6a0f8e0b48a0
kazaneshimizu <ks@rocketa.co.jp>
2024-06-08 13:06:45 +0900

    feat: add title, body request body

--- src/api/useCases/post/updatePost.schema.ts
+++ src/api/useCases/post/updatePost.schema.ts
@@ -5,6 +5,8 @@ export const updatePostUseCaseSchema = z.object({
   projectId: z.string(),
   userId: z.string(),
   status: z.string(),
+  title: z.string().optional(),
+  body: z.string().optional(),
 });
 
 export type UpdatePostUseCaseSchemaType = z.infer<typeof updatePostUseCaseSchema>;
