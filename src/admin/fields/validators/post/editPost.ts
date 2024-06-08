3477c7214431107c305c07e896849a5fba2c4bcc
kazaneshimizu <ks@rocketa.co.jp>
2024-06-08 13:07:42 +0900

    feat: add title and body text fields

--- src/admin/fields/validators/post/editPost.ts
+++ src/admin/fields/validators/post/editPost.ts
@@ -4,11 +4,19 @@ import { yup } from '../../yup.js';
 export type FormValues = {
   slug: string;
   status: string;
+  title?: string;
+  body?: string;
 };
 
 export const editPostValidator = (): ObjectSchema<FormValues> => {
   return yup.object().shape({
     slug: yup.string().required(),
     status: yup.string().required(),
+    title: yup.string().when('status', {
+      is: 'review',
+      then: (schema) => schema.required(),
+      otherwise: (schema) => schema.notRequired(),
+    }),
+    body: yup.string(),
   });
 };
