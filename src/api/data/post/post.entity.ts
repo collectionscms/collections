e33dc783cff28571acf598987ae1d1e53f5cdc75
kazaneshimizu <ks@rocketa.co.jp>
2024-06-22 16:19:31 +0900

    feat: add post status

--- src/api/data/post/post.entity.ts
+++ src/api/data/post/post.entity.ts
@@ -8,6 +8,12 @@ import { FileEntity } from '../file/file.entity.js';
 import { PrismaBaseEntity } from '../prismaBaseEntity.js';
 import { UserEntity } from '../user/user.entity.js';
 
+export const postStatus = {
+  open: 'open',
+  trashed: 'trashed',
+} as const;
+export type PostStatusType = (typeof postStatus)[keyof typeof postStatus];
+
 export class PostEntity extends PrismaBaseEntity<Post> {
   static Construct({
     projectId,
@@ -23,6 +29,7 @@ export class PostEntity extends PrismaBaseEntity<Post> {
       id: postId,
       projectId,
       slug: this.GenerateSlug(),
+      status: postStatus.open,
       createdAt: new Date(),
       updatedAt: new Date(),
     });
