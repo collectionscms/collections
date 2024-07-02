7fadbc7827e25930f9bedad6fedeb617605b72a2
kazaneshimizu <ks@rocketa.co.jp>
2024-07-03 06:44:38 +0900

    feat: add create project validators

---
+++ src/admin/fields/validators/projects/projectSettings.ts
@@ -0,0 +1,14 @@
+import { ObjectSchema } from 'yup';
+import { yup } from '../../yup.js';
+
+export type FormValues = {
+  name: string;
+  subdomain: string;
+};
+
+export const projectSettings = (): ObjectSchema<FormValues> => {
+  return yup.object().shape({
+    name: yup.string().required().max(100),
+    subdomain: yup.string().required(),
+  });
+};
