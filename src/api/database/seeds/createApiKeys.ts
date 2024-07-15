d9c79a89d13b7b862efc98435079b5cb0a41d3cf
kazaneshimizu <ks@rocketa.co.jp>
2024-07-16 07:07:22 +0900

    feat: add create api keys to seed

---
+++ src/api/database/seeds/createApiKeys.ts
@@ -0,0 +1,50 @@
+import { v4 } from 'uuid';
+import { BypassPrismaType } from '../prisma/client.js';
+import { enProject, jaProject } from './createProjects.js';
+import { editorPermissions } from './createRoles.js';
+import { adminUser } from './createUsers.js';
+
+export const enEditorRole = v4();
+export const jaEditorRole = v4();
+
+export const createApiKeys = async (prisma: BypassPrismaType): Promise<void> => {
+  const enProjectApiKeys = [
+    {
+      id: enEditorRole,
+      name: 'Editor',
+      isAdmin: false,
+      projectId: enProject,
+      permissions: editorPermissions,
+    },
+  ];
+
+  const jaProjectApiKeys = [
+    {
+      id: jaEditorRole,
+      name: 'エディター',
+      isAdmin: false,
+      projectId: jaProject,
+      permissions: editorPermissions,
+    },
+  ];
+
+  for (const role of [...enProjectApiKeys, ...jaProjectApiKeys]) {
+    await prisma.apiKey.create({
+      data: {
+        key: v4(),
+        id: role.id,
+        name: role.name,
+        projectId: role.projectId,
+        createdById: adminUser,
+        updatedById: adminUser,
+        apiKeyPermissions: {
+          create: role.permissions.map((permission) => ({
+            id: v4(),
+            projectId: role.projectId,
+            permissionAction: permission,
+          })),
+        },
+      },
+    });
+  }
+};
