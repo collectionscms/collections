c061ac55d89a948a1470b0be388bcd596d7efc3d
kazaneshimizu <ks@rocketa.co.jp>
2024-07-17 07:36:47 +0900

    feat: add findOne

--- src/api/data/apiKey/apiKey.repository.ts
+++ src/api/data/apiKey/apiKey.repository.ts
@@ -3,6 +3,16 @@ import { ProjectPrismaType } from '../../database/prisma/client.js';
 import { ApiKeyEntity } from './apiKey.entity.js';
 
 export class ApiKeyRepository {
+  async findOne(prisma: ProjectPrismaType, id: string): Promise<ApiKeyEntity> {
+    const record = await prisma.apiKey.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
+
+    return ApiKeyEntity.Reconstruct<ApiKey, ApiKeyEntity>(record);
+  }
+
   async findMany(prisma: ProjectPrismaType): Promise<ApiKeyEntity[]> {
     const records = await prisma.apiKey.findMany();
     return records.map((record) => ApiKeyEntity.Reconstruct<ApiKey, ApiKeyEntity>(record));
