ed67481354fb403c1819f68d3e115706115b8822
kazaneshimizu <ks@rocketa.co.jp>
2024-07-17 20:05:55 +0900

    refactor: reduce api key permissions to post only

--- src/api/database/seeds/createPermissions.ts
+++ src/api/database/seeds/createPermissions.ts
@@ -1,16 +1,16 @@
 import { BypassPrismaType } from '../prisma/client.js';
 
-export const createPermissions = async (prisma: BypassPrismaType): Promise<void> => {
-  const actions = {
-    post: ['readPost', 'createPost', 'updatePost', 'trashPost', 'publishPost', 'archivePost'],
-    project: ['readProject', 'updateProject'],
-    role: ['readRole', 'createRole', 'updateRole', 'deleteRole'],
-    user: ['readUser', 'updateUser', 'deleteUser'],
-    invitation: ['inviteUser'],
-    review: ['readOwnReview', 'readAllReview', 'createReview', 'approveReview', 'closeReview'],
-    apiKey: ['createApiKey', 'readApiKey', 'updateApiKey', 'deleteApiKey'],
-  };
+export const actions = {
+  post: ['readPost', 'createPost', 'updatePost', 'trashPost', 'publishPost', 'archivePost'],
+  project: ['readProject', 'updateProject'],
+  role: ['readRole', 'createRole', 'updateRole', 'deleteRole'],
+  user: ['readUser', 'updateUser', 'deleteUser'],
+  invitation: ['inviteUser'],
+  review: ['readOwnReview', 'readAllReview', 'createReview', 'approveReview', 'closeReview'],
+  apiKey: ['createApiKey', 'readApiKey', 'updateApiKey', 'deleteApiKey'],
+};
 
+export const createPermissions = async (prisma: BypassPrismaType): Promise<void> => {
   await prisma.permission.createMany({
     data: [
       ...Object.entries(actions).flatMap(([group, actions]) =>
