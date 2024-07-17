b523e101cf0f52fac7f856860fe9f011684a5bae
kazaneshimizu <ks@rocketa.co.jp>
2024-07-18 08:46:50 +0900

    feat: add api key permission

---
+++ src/api/data/apiKeyPermission/apiKeyPermission.entity.ts
@@ -0,0 +1,43 @@
+import { ApiKeyPermission } from '@prisma/client';
+import { v4 } from 'uuid';
+import { UnexpectedException } from '../../../exceptions/unexpected.js';
+import { PrismaBaseEntity } from '../prismaBaseEntity.js';
+
+export class ApiKeyPermissionEntity extends PrismaBaseEntity<ApiKeyPermission> {
+  static Construct({
+    apiKeyId,
+    projectId,
+    permissionAction,
+  }: {
+    apiKeyId: string;
+    projectId: string;
+    permissionAction: string;
+  }): ApiKeyPermissionEntity {
+    return new ApiKeyPermissionEntity({
+      id: v4(),
+      projectId,
+      apiKeyId,
+      permissionAction,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+
+  get id(): string {
+    return this.props.id;
+  }
+
+  private isValid() {
+    if (!this.props.id) {
+      throw new UnexpectedException({ message: 'id is required' });
+    }
+  }
+
+  public beforeUpdateValidate(): void {
+    this.isValid();
+  }
+
+  public beforeInsertValidate(): void {
+    this.isValid();
+  }
+}
