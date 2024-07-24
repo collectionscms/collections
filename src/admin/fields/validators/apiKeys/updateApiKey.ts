0ed0a2865105b811a9ad0afd7bab7e7e7ecf8327
kazaneshimizu <ks@rocketa.co.jp>
2024-07-25 06:56:27 +0900

    feat: add permission checkbox to input form

--- src/admin/fields/validators/apiKeys/updateApiKey.ts
+++ src/admin/fields/validators/apiKeys/updateApiKey.ts
@@ -4,11 +4,13 @@ import { yup } from '../../yup.js';
 export type FormValues = {
   name: string;
   key?: string;
+  permissions: string[];
 };
 
 export const updateApiKeySchema = (): ObjectSchema<FormValues> => {
   return yup.object().shape({
     name: yup.string().required().max(60),
     key: yup.string().optional().max(250),
+    permissions: yup.array().of(yup.string().required()).min(1).required(),
   });
 };
--- src/admin/pages/ApiKey/Create/index.tsx
+++ src/admin/pages/ApiKey/Create/index.tsx
@@ -1,5 +1,6 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import {
+  Box,
   Button,
   Checkbox,
   FormControlLabel,
@@ -94,64 +95,66 @@ const CreateApiKeyPageImpl: React.FC = () => {
                     <FormHelperText error>{errors.name?.message}</FormHelperText>
                   </Stack>
                 </Grid>
-              </Grid>
-              <InputLabel sx={{ my: 1 }}>{t('permission')}</InputLabel>
-              <Grid container spacing={3}>
-                <Controller
-                  name="permissions"
-                  control={control}
-                  render={({ field }) => (
-                    <>
-                      {Object.values(permissions).map((permission) => {
-                        return (
-                          <Grid xs={12} sm={6} key={permission}>
-                            <Stack>
-                              <FormControlLabel
-                                {...field}
-                                value={permission}
-                                control={
-                                  <Checkbox
+                <Grid xs={12}>
+                  <InputLabel sx={{ my: 1 }}>{t('permission')}</InputLabel>
+                  <Grid container spacing={3}>
+                    <Controller
+                      name="permissions"
+                      control={control}
+                      render={({ field }) => (
+                        <>
+                          {Object.values(permissions).map((permission) => {
+                            return (
+                              <Grid xs={12} sm={6} key={permission}>
+                                <Stack>
+                                  <FormControlLabel
                                     {...field}
-                                    checked={watch('permissions').includes(permission)}
-                                    onChange={() => {
-                                      if (!field.value.includes(permission)) {
-                                        field.onChange([...field.value, permission]);
-                                        return;
-                                      }
-                                      const newTopics = field.value.filter(
-                                        (topic) => topic !== permission
-                                      );
-                                      field.onChange(newTopics);
-                                    }}
+                                    value={permission}
+                                    control={
+                                      <Checkbox
+                                        {...field}
+                                        checked={watch('permissions').includes(permission)}
+                                        onChange={() => {
+                                          if (!field.value.includes(permission)) {
+                                            field.onChange([...field.value, permission]);
+                                            return;
+                                          }
+                                          const newTopics = field.value.filter(
+                                            (topic) => topic !== permission
+                                          );
+                                          field.onChange(newTopics);
+                                        }}
+                                      />
+                                    }
+                                    label={t(
+                                      `permissions.${permission}` as unknown as TemplateStringsArray
+                                    )}
                                   />
-                                }
-                                label={t(
-                                  `permissions.${permission}` as unknown as TemplateStringsArray
-                                )}
-                              />
-                              <Typography variant="subtitle2" color="secondary" sx={{ ml: 3 }}>
-                                {t(
-                                  `permissions.${permission}_description` as unknown as TemplateStringsArray
-                                )}
-                              </Typography>
-                            </Stack>
-                          </Grid>
-                        );
-                      })}
-                    </>
-                  )}
-                />
-              </Grid>
-              <FormHelperText error>{errors.permissions?.message}</FormHelperText>
-              <Grid xs={12}>
-                <Stack direction="row" justifyContent="flex-end" spacing={1}>
-                  <Button variant="outlined" color="secondary" onClick={navigateToList}>
-                    {t('cancel')}
-                  </Button>
-                  <Button variant="contained" type="submit" disabled={isMutating}>
-                    {t('save')}
-                  </Button>
-                </Stack>
+                                  <Typography variant="subtitle2" color="secondary" sx={{ ml: 3 }}>
+                                    {t(
+                                      `permissions.${permission}_description` as unknown as TemplateStringsArray
+                                    )}
+                                  </Typography>
+                                </Stack>
+                              </Grid>
+                            );
+                          })}
+                        </>
+                      )}
+                    />
+                  </Grid>
+                  <FormHelperText error>{errors.permissions?.message}</FormHelperText>
+                </Grid>
+                <Grid xs={12}>
+                  <Stack direction="row" justifyContent="flex-end" spacing={1}>
+                    <Button variant="outlined" color="secondary" onClick={navigateToList}>
+                      {t('cancel')}
+                    </Button>
+                    <Button variant="contained" type="submit" disabled={isMutating}>
+                      {t('save')}
+                    </Button>
+                  </Stack>
+                </Grid>
               </Grid>
             </form>
           </MainCard>
