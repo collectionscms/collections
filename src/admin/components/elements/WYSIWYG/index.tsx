dad86dab397fa4f900f2282eb6f0de6eca020acc
kazaneshimizu <ks@rocketa.co.jp>
2024-07-28 08:23:58 +0900

    feat: add code / code block

--- src/admin/components/elements/WYSIWYG/index.tsx
+++ src/admin/components/elements/WYSIWYG/index.tsx
@@ -1,10 +1,14 @@
-import {
-  BoldOutlined,
-  ItalicOutlined,
-  StrikethroughOutlined,
-  UnderlineOutlined,
-} from '@ant-design/icons';
 import { Paper, Stack } from '@mui/material';
+import {
+  RiBold,
+  RiCodeBlock,
+  RiCodeLine,
+  RiCodeLine,
+  RiCodeSSlashLine,
+  RiItalic,
+  RiStrikethrough,
+  RiUnderline,
+} from '@remixicon/react';
 import { BubbleMenu, Editor, EditorContent } from '@tiptap/react';
 import React from 'react';
 import { IconButton } from '../../../@extended/components/IconButton/index.js';
@@ -27,35 +31,49 @@ export const WYSIWYG: React.FC<Props> = ({ editor }) => {
       <EditorContent editor={editor} />
       {editor && (
         <BubbleMenu className="bubble-menu" tippyOptions={{ duration: 100 }} editor={editor}>
-          <Paper elevation={1} sx={{ p: 0.2 }}>
+          <Paper elevation={1} sx={{ p: 0.2, borderRadius: 1 }}>
             <Stack direction="row">
               <IconButton
                 onClick={() => editor.chain().focus().toggleBold().run()}
                 color="secondary"
                 size="small"
               >
-                <BoldOutlined style={{ fontSize: 16 }} />
+                <RiBold size={18} />
               </IconButton>
               <IconButton
                 onClick={() => editor.chain().focus().toggleItalic().run()}
                 color="secondary"
                 size="small"
               >
-                <ItalicOutlined style={{ fontSize: 16 }} />
+                <RiItalic size={18} />
               </IconButton>
               <IconButton
                 onClick={() => editor.chain().focus().toggleUnderline().run()}
                 color="secondary"
                 size="small"
               >
-                <UnderlineOutlined style={{ fontSize: 16 }} />
+                <RiUnderline size={18} />
               </IconButton>
               <IconButton
                 onClick={() => editor.chain().focus().toggleStrike().run()}
                 color="secondary"
                 size="small"
               >
-                <StrikethroughOutlined style={{ fontSize: 16 }} />
+                <RiStrikethrough size={18} />
+              </IconButton>
+              <IconButton
+                onClick={() => editor.chain().focus().toggleCode().run()}
+                color="secondary"
+                size="small"
+              >
+                <RiCodeLine size={18} />
+              </IconButton>
+              <IconButton
+                onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+                color="secondary"
+                size="small"
+              >
+                <RiCodeSSlashLine size={18} />
               </IconButton>
             </Stack>
           </Paper>
