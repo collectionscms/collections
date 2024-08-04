5e1747a012321a55ab6b43c9f6ff0c97f7dd0b4c
kazaneshimizu <ks@rocketa.co.jp>
2024-08-05 07:40:24 +0900

    feat: add content item menu

--- src/admin/components/elements/BlockEditor/menus/ContentItemMenu/index.tsx
+++ src/admin/components/elements/BlockEditor/menus/ContentItemMenu/index.tsx
@@ -1,20 +1,33 @@
-import { Stack } from '@mui/material';
+import { Paper, Stack, Typography } from '@mui/material';
+import * as Popover from '@radix-ui/react-popover';
 import DragHandle from '@tiptap-pro/extension-drag-handle-react';
 import { Editor } from '@tiptap/react';
-import React, { useState } from 'react';
-import { ToolbarButton } from '../../ToolbarButton/index.js';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { DropdownButton } from '../../ui/DropdownButton/index.js';
 import { Icon } from '../../ui/Icon/index.js';
+import { ToolbarButton } from '../../ui/ToolbarButton/index.js';
 import { useContentItemActions } from './hooks/useContentItemActions.js';
 import { useData } from './hooks/useData.js';
+import './styles/index.css';
 
 export type Props = {
   editor: Editor;
 };
 
 export const ContentItemMenu: React.FC<Props> = ({ editor }) => {
+  const { t } = useTranslation();
   const [menuOpen, setMenuOpen] = useState(false);
   const data = useData();
   const actions = useContentItemActions(editor, data.currentNode, data.currentNodePos);
+
+  useEffect(() => {
+    if (menuOpen) {
+      editor.commands.setMeta('lockDragHandle', true);
+    } else {
+      editor.commands.setMeta('lockDragHandle', false);
+    }
+  }, [editor, menuOpen]);
 
   return (
     <DragHandle
@@ -27,9 +40,53 @@ export const ContentItemMenu: React.FC<Props> = ({ editor }) => {
       }}
     >
       <Stack direction="row" alignItems="center" gap={0.5}>
-        <ToolbarButton onClick={actions.handleAdd} tooltip="Add content item">
+        <ToolbarButton onClick={actions.handleAdd}>
           <Icon name="Plus" />
         </ToolbarButton>
+        <Popover.Root open={menuOpen} onOpenChange={setMenuOpen}>
+          <Popover.Trigger asChild>
+            <ToolbarButton>
+              <Icon name="GripVertical" />
+            </ToolbarButton>
+          </Popover.Trigger>
+          <Popover.Content side="bottom" align="start">
+            <Paper sx={{ p: 1 }}>
+              <Stack direction="column">
+                <DropdownButton
+                  onClick={() => {
+                    setMenuOpen(false);
+                    actions.duplicateNode();
+                  }}
+                >
+                  <Icon name="Copy" size={18} />
+                  <Typography sx={{ ml: 1 }}>{t('editor.duplicate')}</Typography>
+                </DropdownButton>
+                <DropdownButton
+                  onClick={() => {
+                    setMenuOpen(false);
+                    actions.copyNodeToClipboard();
+                  }}
+                >
+                  <Icon name="Clipboard" size={18} />
+                  <Typography sx={{ ml: 1 }}>{t('editor.copy_to_clipboard')}</Typography>
+                </DropdownButton>
+                <DropdownButton
+                  color="error"
+                  onClick={() => {
+                    setMenuOpen(false);
+                    actions.deleteNode();
+                  }}
+                  sx={{
+                    color: 'error.main',
+                  }}
+                >
+                  <Icon name="Trash2" size={18} />
+                  <Typography sx={{ ml: 1 }}>{t('delete')}</Typography>
+                </DropdownButton>
+              </Stack>
+            </Paper>
+          </Popover.Content>
+        </Popover.Root>
       </Stack>
     </DragHandle>
   );
