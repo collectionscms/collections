8c1ad47a631219de4ec3906a7e7eb8f8c4488bdf
kazaneshimizu <ks@rocketa.co.jp>
2024-08-05 09:44:35 +0900

    style: add strokeWidth

--- src/admin/components/elements/BlockEditor/ui/Icon/index.tsx
+++ src/admin/components/elements/BlockEditor/ui/Icon/index.tsx
@@ -4,16 +4,17 @@ import React, { memo } from 'react';
 export type Props = {
   name: keyof typeof icons;
   size?: number;
+  strokeWidth?: number;
 };
 
-export const Icon = memo(({ name, size }: Props) => {
+export const Icon = memo(({ name, size, strokeWidth }: Props) => {
   const IconComponent = icons[name];
 
   if (!IconComponent) {
     return null;
   }
 
-  return <IconComponent size={size} strokeWidth={1.5} />;
+  return <IconComponent size={size} strokeWidth={strokeWidth || 1.5} />;
 });
 
 Icon.displayName = 'Icon';
