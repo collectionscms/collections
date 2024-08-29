a91e82c787f997f76738c9edc22cff663a91c467
kazaneshimizu <ks@rocketa.co.jp>
2024-08-29 09:46:22 +0900

    feat: add translationEnabled column to project

--- prisma/migrations/20240829004502_add_init_schema/migration.sql
+++ prisma/migrations/20240829004502_add_init_schema/migration.sql
@@ -6,6 +6,7 @@ CREATE TABLE "Project" (
     "subdomain" VARCHAR(255) NOT NULL,
     "iconUrl" VARCHAR(255),
     "enabled" BOOLEAN NOT NULL DEFAULT true,
+    "translationEnabled" BOOLEAN NOT NULL DEFAULT true,
     "sourceLanguage" VARCHAR(255) NOT NULL,
     "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMPTZ(6) NOT NULL,
