33df1c36554a534cbe64fd8e4cb5b11405090c57
kazaneshimizu <ks@rocketa.co.jp>
2024-09-15 12:27:33 +0900

    feat: add create

--- src/api/persistence/webhookLog/webhookLog.repository.ts
+++ src/api/persistence/webhookLog/webhookLog.repository.ts
@@ -1,1 +1,16 @@
-export class WebhookLogRepository {}
+import { WebhookLog } from '@prisma/client';
+import { ProjectPrismaType } from '../../database/prisma/client.js';
+import { WebhookLogEntity } from './webhookLog.entity.js';
+
+export class WebhookLogRepository {
+  async create(prisma: ProjectPrismaType, entity: WebhookLogEntity): Promise<WebhookLogEntity> {
+    entity.beforeInsertValidate();
+
+    const result = await prisma.webhookLog.create({
+      data: {
+        ...entity.toPersistence(),
+      },
+    });
+    return WebhookLogEntity.Reconstruct<WebhookLog, WebhookLogEntity>(result);
+  }
+}
