6e474250ac95f06f9ab6d538530d2041ea72d679
kazaneshimizu <ks@rocketa.co.jp>
2024-10-07 09:24:57 +0900

    feat: add onRevert to webhookSetting

--- prisma/migrations/20241007002301_add_init_schema/migration.sql
+++ prisma/migrations/20241007002301_add_init_schema/migration.sql
@@ -225,6 +225,7 @@ CREATE TABLE "WebhookSetting" (
     "onArchive" BOOLEAN NOT NULL DEFAULT false,
     "onDeletePublished" BOOLEAN NOT NULL DEFAULT false,
     "onRestorePublished" BOOLEAN NOT NULL DEFAULT false,
+    "onRevert" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMPTZ(6) NOT NULL,
 
