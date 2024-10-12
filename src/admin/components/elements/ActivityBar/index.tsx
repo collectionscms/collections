5ec7aef215df2d4d8cd4a5364d68f26e3c962fe2
kazaneshimizu <ks@rocketa.co.jp>
2024-10-12 15:01:24 +0900

    feat: add project list menu

--- src/admin/components/elements/ActivityBar/index.tsx
+++ src/admin/components/elements/ActivityBar/index.tsx
@@ -4,14 +4,12 @@ import React, { useMemo } from 'react';
 import { Avatar } from '../../../@extended/components/Avatar/index.js';
 import { getAddNewProjectUrl, getUrlForTenant } from '../../../utilities/urlGenerator.js';
 import { useAuth } from '../../utilities/Auth/index.js';
-import { useColorMode } from '../../utilities/ColorMode/index.js';
 import { Icon } from '../Icon/index.js';
 import { Link } from '../Link/index.js';
 import { BottomContent } from '../NavContent/BottomContent/index.js';
 
 export const ActivityBar: React.FC = () => {
   const theme = useTheme();
-  const colorMode = useColorMode();
   const { currentProjectRole, projects } = useAuth();
   const bottomContent = useMemo(() => <BottomContent />, []);
 
