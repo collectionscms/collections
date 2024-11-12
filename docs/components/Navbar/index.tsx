ea22bddbfa4f1b9246e8889d2ae69c2ef288ec2d
kazaneshimizu <ks@rocketa.co.jp>
2024-11-13 06:20:52 +0900

    feat: add language switch to header

--- docs/components/Navbar/index.tsx
+++ docs/components/Navbar/index.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 import logo from '../../assets/images/logo.svg';
+import { LanguageSwitcher } from '../LanguageSwitcher';
 import { NavbarMenu } from './NavbarMenu';
-import { useRouter } from 'next/router';
 
 export const Navbar: React.FC = () => {
   const { locale } = useRouter();
@@ -11,13 +12,16 @@ export const Navbar: React.FC = () => {
   const signUpTitle = locale === 'ja' ? '利用登録' : 'Try for free';
 
   return (
-    <div className="bg-black text-white py-6 px-14 border-b-neutral-400 border-b">
-      <div className="flex flex-row items-center gap-12">
+    <div className="flex bg-black text-white py-4 pl-14 pr-6 border-b-neutral-400 border-b">
+      <div className="flex flex-row items-center gap-12 flex-grow">
         <Link href="/">
           <Image src={logo} alt="logo" className="w-36" />
         </Link>
         <NavbarMenu href="https://app.collectionsdemo.live/admin/" title={demoTitle} />
         <NavbarMenu href="https://app.collections.dev/" title={signUpTitle} variant="primary" />
+      </div>
+      <div>
+        <LanguageSwitcher />
       </div>
     </div>
   );
