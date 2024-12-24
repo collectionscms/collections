f8cea12cd73db22b5a7845fcfda63aff84171d1a
kazaneshimizu <ks@rocketa.co.jp>
2024-12-24 09:39:30 +0900

    feat: add api preview button to post footer

--- src/admin/pages/Post/Edit/PostFooter/index.tsx
+++ src/admin/pages/Post/Edit/PostFooter/index.tsx
@@ -1,6 +1,10 @@
-import { Stack } from '@mui/material';
-import React from 'react';
+import { Stack, useTheme } from '@mui/material';
+import { ApiKey } from '@prisma/client';
+import React, { useEffect, useState } from 'react';
 import { RevisedContent } from '../../../../../types/index.js';
+import { ApiPreview } from '../../../../components/elements/ApiPreview/index.js';
+import { useAuth } from '../../../../components/utilities/Auth/index.js';
+import { usePost } from '../../Context/index.js';
 import { Information } from './Information/index.js';
 import { Revision } from './Revision/index.js';
 import { Settings } from './Settings/index.js';
@@ -13,6 +17,23 @@ export type Props = {
 };
 
 export const PostFooter: React.FC<Props> = ({ content, characters, onTrashed, onReverted }) => {
+  const theme = useTheme();
+  const { hasPermission } = useAuth();
+  const { getApiKeys } = usePost();
+  const { trigger: getApiKeyTrigger } = getApiKeys();
+
+  const [apiKeys, setApiKeys] = useState<ApiKey[]>([]);
+  useEffect(() => {
+    const fetchApiKeys = async () => {
+      const apiKeys = await getApiKeyTrigger();
+      setApiKeys(apiKeys);
+    };
+
+    if (hasPermission('readApiKey')) {
+      fetchApiKeys();
+    }
+  }, []);
+
   return (
     <Stack
       flexDirection="row"
@@ -29,6 +50,20 @@ export const PostFooter: React.FC<Props> = ({ content, characters, onTrashed, on
         <Information characters={characters} />
       </Stack>
       <Stack flexDirection="row" gap={2}>
+        <ApiPreview
+          path={`contents/${content.slug}`}
+          apiKeys={apiKeys}
+          buttonProps={{
+            color: 'secondary',
+            shape: 'rounded',
+            variant: 'contained',
+            sx: {
+              color: 'text.primary',
+              backgroundColor: theme.palette.grey[200],
+              '&:hover': { backgroundColor: theme.palette.grey[300] },
+            },
+          }}
+        />
         <Settings content={content} onTrashed={onTrashed} />
       </Stack>
     </Stack>
