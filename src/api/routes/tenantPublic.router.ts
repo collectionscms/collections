a933ce2b0b3248685db35cb156e557dad0ee5007
kazaneshimizu <ks@rocketa.co.jp>
2024-12-25 09:06:18 +0900

    feat: add endpoint to fetch published contents from tag name

--- src/api/routes/tenantPublic.router.ts
+++ src/api/routes/tenantPublic.router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { content } from './public/v1/content.router.js';
 import { post } from './public/v1/post.router.js';
+import { tag } from './public/v1/tag.router.js';
 
 export const tenantPublicApiRouter = express.Router();
 
 tenantPublicApiRouter.use(post);
 tenantPublicApiRouter.use(content);
+tenantPublicApiRouter.use(tag);
---
+++ src/api/useCases/tag/getTagPublishedListContents.useCase.ts
@@ -0,0 +1,34 @@
+import { RecordNotFoundException } from '../../../exceptions/database/recordNotFound.js';
+import { PublishedListContent } from '../../../types/index.js';
+import { ProjectPrismaType } from '../../database/prisma/client.js';
+import { ContentTagRepository } from '../../persistence/contentTag/contentTag.repository.js';
+import { TagRepository } from '../../persistence/tag/tag.repository.js';
+import { GetTagPublishedListContentsUseCaseSchemaType } from './getTagPublishedListContents.useCase.schema.js';
+
+export class GetTagPublishedListContentsUseCase {
+  constructor(
+    private readonly prisma: ProjectPrismaType,
+    private readonly tagRepository: TagRepository,
+    private readonly contentTagRepository: ContentTagRepository
+  ) {}
+
+  async execute({
+    language,
+    name,
+  }: GetTagPublishedListContentsUseCaseSchemaType): Promise<PublishedListContent[]> {
+    const tag = await this.tagRepository.findOneByName(this.prisma, name);
+    if (!tag) {
+      throw new RecordNotFoundException('record_not_found');
+    }
+
+    const records = await this.contentTagRepository.findPublishedContentsByTagId(
+      this.prisma,
+      tag.id
+    );
+    const filteredRecords = records.filter((record) => record.content.language === language);
+
+    return filteredRecords
+      .map((record) => record.content.toPublishedListContentResponse(record.createdBy))
+      .sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+  }
+}
