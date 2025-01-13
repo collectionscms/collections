37db0db7dfcdb473640b8841157e128b0be1caba
kazaneshimizu <ks@rocketa.co.jp>
2025-01-13 19:52:42 +0900

    test: change testMatch

--- jest.integrations.config.mjs
+++ jest.integrations.config.mjs
@@ -3,6 +3,6 @@ import jestBaseConfig from './jest.config.mjs';
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   ...jestBaseConfig,
-  testMatch: ['**/api/**/*.int-test.ts'],
+  testMatch: ['**/api/**/*.int.test.ts'],
   testPathIgnorePatterns: [],
 };
