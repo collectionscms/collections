1edc6a8f17f225677fb870d8ccf46670821559e7
kazaneshimizu <ks@rocketa.co.jp>
2025-01-13 09:51:10 +0900

    test: add integration tests

---
+++ jest.integrations.config.mjs
@@ -0,0 +1,7 @@
+import jestBaseConfig from './jest.config.mjs';
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  ...jestBaseConfig,
+  testMatch: ['**/api/**/*int.test.ts'],
+};
---
+++ tests/common/variables.ts
@@ -0,0 +1,9 @@
+export const project = {
+  host: 'en.test.com',
+};
+
+export const apiKey = {
+  fullAccess: {
+    key: 'bd7ef64b-dc9b-4d42-a486-df1ebd69fe1b',
+  },
+};
