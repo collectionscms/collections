559bff33924875b21fd8c02d2cbc803cb5a118a6
kazaneshimizu <ks@rocketa.co.jp>
2025-01-25 11:37:36 +0900

    test: add msw

--- packages/text-editor/.storybook/preview.ts
+++ packages/text-editor/.storybook/preview.ts
@@ -1,14 +1,10 @@
-import type { Preview } from '@storybook/react';
+import { initialize, mswLoader } from 'msw-storybook-addon';
 
-const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
-  },
+initialize();
+
+const preview = {
+  parameters: {},
+  loaders: [mswLoader],
 };
 
 export default preview;
