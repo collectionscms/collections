e0ab932d2ef57de3ee7dca0b5d0a10709ddc8d57
kazaneshimizu <ks@rocketa.co.jp>
2025-01-25 16:04:26 +0900

    feat: add api request

--- packages/text-editor/src/text-editor/index.stories.tsx
+++ packages/text-editor/src/text-editor/index.stories.tsx
@@ -1,19 +1,51 @@
 import { Meta, StoryFn } from '@storybook/react';
+import { http, HttpResponse } from 'msw';
 import React from 'react';
 import { TextEditor } from './index';
 
 export default {
   title: 'Components/TextEditor',
   component: TextEditor,
+  parameters: {
+    msw: {
+      handlers: [
+        http.get('https://en.collections.dev/api/v1/contents/:contentId', () => {
+          return HttpResponse.json({
+            content: {
+              bodyJson: {
+                type: 'doc',
+                content: [
+                  {
+                    type: 'paragraph',
+                    content: [{ type: 'text', text: 'Mock Content' }],
+                  },
+                ],
+              },
+            },
+          });
+        }),
+        http.post('https://en.collections.dev/api/v1/contents', () => {
+          return HttpResponse.json({
+            content: {
+              id: '00000000-0000-0000-0000-000000000000',
+            },
+          });
+        }),
+        http.put('https://en.collections.dev/api/v1/contents/:contentId', () => {
+          return HttpResponse.json({});
+        }),
+      ],
+    },
+  },
 } as Meta<typeof TextEditor>;
 
 const Template: StoryFn<typeof TextEditor> = (args) => <TextEditor {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
-  projectId: '123',
-  slug: 'example',
+  projectId: 'en',
   apiKey: '123',
+  id: '00000000-0000-0000-0000-000000000000',
   options: {
     language: 'en',
   },
--- packages/text-editor/src/text-editor/index.tsx
+++ packages/text-editor/src/text-editor/index.tsx
@@ -1,6 +1,6 @@
 import { Placeholder } from '@tiptap/extension-placeholder';
 import { EditorContent } from '@tiptap/react';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useBlockEditor } from './hooks/useBlockEditor';
 import i18n from './locales/localization';
@@ -8,15 +8,76 @@ import './styles/index.css';
 
 type Props = {
   projectId: string;
-  slug: string;
   apiKey: string;
+  id: string | null;
   options?: {
     language?: 'en' | 'ja';
   };
+  onInit?: (id: string) => void;
 };
 
-export const TextEditor = ({ projectId, slug, apiKey, options = { language: 'en' } }: Props) => {
+export const TextEditor = ({
+  projectId,
+  apiKey,
+  id,
+  options = { language: 'en' },
+  onInit,
+}: Props) => {
   const { t } = useTranslation();
+  const [isDirty, setIsDirty] = useState(false);
+  const [contentId, setContentId] = useState(id);
+
+  const fetchContent = async () => {
+    try {
+      const res = await fetch(`https://${projectId}.collections.dev/api/v1/contents/${contentId}`, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      });
+      const data = await res.json();
+      editor?.commands.setContent(data.content.bodyJson);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const createContent = async () => {
+    try {
+      const res = await fetch(`https://${projectId}.collections.dev/api/v1/contents`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      });
+      const data = await res.json();
+      setContentId(data.content.id);
+      onInit?.(data.content.id);
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const updateContent = async (
+    body: string | null,
+    bodyHtml: string | null,
+    bodyJson: string | null
+  ) => {
+    try {
+      await fetch(`https://${projectId}.collections.dev/api/v1/contents/${contentId}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          body,
+          bodyHtml,
+          bodyJson,
+        }),
+      });
+    } catch (error) {
+      throw error;
+    }
+  };
 
   const { editor, characterCount } = useBlockEditor({
     initialContent: '',
@@ -27,8 +88,18 @@ export const TextEditor = ({ projectId, slug, apiKey, options = { language: 'en'
     ],
   });
 
+  // on mount
   useEffect(() => {
-    i18n.changeLanguage(options.language);
+    if (id) {
+      fetchContent();
+    } else {
+      createContent();
+    }
+
+    if (options.language) {
+      i18n.changeLanguage(options.language);
+    }
+
     const style = document.createElement('style');
     style.textContent = `
       .ProseMirror {
@@ -45,11 +116,41 @@ export const TextEditor = ({ projectId, slug, apiKey, options = { language: 'en'
     return () => {
       document.head.removeChild(style);
     };
-  }, [options.language]);
+  }, []);
 
   useEffect(() => {
-    console.log('changed', editor?.getText());
-  }, [editor?.getText()]);
+    if (isDirty) {
+      // auto save after 5 seconds
+      const timer = setTimeout(async () => {
+        await updateContent(
+          editor?.getText() ?? null,
+          editor?.getHTML() ?? null,
+          JSON.stringify(editor?.getJSON()) ?? null
+        );
+        setIsDirty(false);
+      }, 5_000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [editor?.getHTML()]);
+
+  useEffect(() => {
+    if (editor) {
+      const handleUpdate = () => {
+        setIsDirty(true);
+      };
+
+      // Set the cursor to the beginning of the editor
+      editor.commands.setTextSelection(0);
+      editor.view.focus();
+
+      editor.on('update', handleUpdate);
+
+      return () => {
+        editor.off('update', handleUpdate);
+      };
+    }
+  }, [editor]);
 
   return (
     <>
