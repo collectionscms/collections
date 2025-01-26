b6c29fc33876b51047dc23d951e592e6eadaf755
kazaneshimizu <ks@rocketa.co.jp>
2025-01-26 13:47:30 +0900

    feat: add conte updating endpoint for api

--- src/api/routes/public/v1/content.router.ts
+++ src/api/routes/public/v1/content.router.ts
@@ -7,8 +7,11 @@ import { validateAccess } from '../../../middlewares/validateAccess.js';
 import { ContentRepository } from '../../../persistence/content/content.repository.js';
 import { ContentRevisionRepository } from '../../../persistence/contentRevision/contentRevision.repository.js';
 import { ContentTagRepository } from '../../../persistence/contentTag/contentTag.repository.js';
+import { PostRepository } from '../../../persistence/post/post.repository.js';
 import { GetPublishedContentUseCase } from '../../../useCases/content/getPublishedContent.useCase.js';
 import { getPublishedContentUseCaseSchema } from '../../../useCases/content/getPublishedContent.useCase.schema.js';
+import { UpdateContentUseCase } from '../../../useCases/content/updateContent.useCase.js';
+import { updateContentUseCaseSchema } from '../../../useCases/content/updateContent.useCase.schema.js';
 
 const router = express.Router();
 
@@ -34,6 +37,38 @@ router.get(
     const content = await useCase.execute(validated.data);
 
     res.json({ content });
+  })
+);
+
+router.patch(
+  '/contents/:id',
+  authenticatedUser,
+  validateAccess(['savePost']),
+  asyncHandler(async (req: Request, res: Response) => {
+    const validated = updateContentUseCaseSchema.safeParse({
+      projectId: res.projectRole?.id,
+      id: req.params.id,
+      userId: res.user.id,
+      title: req.body.title,
+      subtitle: req.body.subtitle,
+      body: req.body.body,
+      bodyJson: req.body.bodyJson,
+      bodyHtml: req.body.bodyHtml,
+      coverUrl: req.body.coverUrl,
+      slug: req.body.slug,
+      metaTitle: req.body.metaTitle,
+      metaDescription: req.body.metaDescription,
+    });
+    if (!validated.success) throw new InvalidPayloadException('bad_request', validated.error);
+
+    const useCase = new UpdateContentUseCase(
+      projectPrisma(validated.data.projectId),
+      new ContentRevisionRepository(),
+      new PostRepository()
+    );
+    await useCase.execute(validated.data);
+
+    res.status(204).send();
   })
 );
 
