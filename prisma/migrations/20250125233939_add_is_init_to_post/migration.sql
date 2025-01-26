bfeb8722d4b6ccf575ee830cefe7ab23159055ca
kazaneshimizu <ks@rocketa.co.jp>
2025-01-26 09:18:26 +0900

    refactor: delete default for draft key

--- prisma/migrations/20250125233939_add_is_init_to_post/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Content" ALTER COLUMN "draftKey" SET DEFAULT substring(md5(random()::text), 1, 10);
-
--- AlterTable
-ALTER TABLE "ContentRevision" ALTER COLUMN "draftKey" SET DEFAULT substring(md5(random()::text), 1, 10);
-
--- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "isInit" BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20250126001605_add_is_init_to_post/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "isInit" BOOLEAN NOT NULL DEFAULT false;
