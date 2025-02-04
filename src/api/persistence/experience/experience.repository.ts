016cb16b6e15ccda1ce07518a683d3f9423ed5c2
kazaneshimizu <ks@rocketa.co.jp>
2025-02-05 08:54:51 +0900

    feat: implement get experiences end point

--- src/api/persistence/experience/experience.repository.ts
+++ src/api/persistence/experience/experience.repository.ts
@@ -1,7 +1,29 @@
+import { Experience, ExperienceResource } from '@prisma/client';
 import { ProjectPrismaType } from '../../database/prisma/client.js';
+import { ExperienceResourceEntity } from '../experienceResource/experienceResource.entity.js';
 import { ExperienceEntity } from './experience.entity.js';
 
 export class ExperienceRepository {
+  async findManyWithResources(prisma: ProjectPrismaType): Promise<
+    {
+      experience: ExperienceEntity;
+      resources: ExperienceResourceEntity[];
+    }[]
+  > {
+    const experiences = await prisma.experience.findMany({
+      include: {
+        experienceResources: true,
+      },
+    });
+
+    return experiences.map((experience) => ({
+      experience: ExperienceEntity.Reconstruct<Experience, ExperienceEntity>(experience),
+      resources: experience.experienceResources.map((resource) =>
+        ExperienceResourceEntity.Reconstruct<ExperienceResource, ExperienceResourceEntity>(resource)
+      ),
+    }));
+  }
+
   async createMany(prisma: ProjectPrismaType, experiences: ExperienceEntity[]): Promise<void> {
     await prisma.experience.createMany({
       data: experiences.map((experience) => experience.toPersistence()),
--- src/api/routes/experience.router.ts
+++ src/api/routes/experience.router.ts
@@ -5,10 +5,33 @@ import { asyncHandler } from '../middlewares/asyncHandler.js';
 import { authenticatedUser } from '../middlewares/auth.js';
 import { validateAccess } from '../middlewares/validateAccess.js';
 import { ExperienceRepository } from '../persistence/experience/experience.repository.js';
+import { ExperienceResourceRepository } from '../persistence/experienceResource/experienceResource.repository.js';
 import { CreateExperienceUseCase } from '../useCases/experience/createExperience.useCase.js';
 import { createExperienceUseCaseSchema } from '../useCases/experience/createExperience.useCase.schema.js';
+import { GetExperiencesUseCase } from '../useCases/experience/getExperiences.useCase.js';
+import { getExperiencesUseCaseSchema } from '../useCases/experience/getExperiences.useCase.schema.js';
 
 const router = express.Router();
+
+router.get(
+  '/experiences',
+  authenticatedUser,
+  validateAccess(['readSeo']),
+  asyncHandler(async (req: Request, res: Response) => {
+    const validated = getExperiencesUseCaseSchema.safeParse({
+      projectId: res.projectRole?.id,
+    });
+    if (!validated.success) throw new InvalidPayloadException('bad_request', validated.error);
+
+    const useCase = new GetExperiencesUseCase(
+      projectPrisma(validated.data.projectId),
+      new ExperienceRepository()
+    );
+    const experiences = await useCase.execute();
+
+    res.json({ experiences });
+  })
+);
 
 router.post(
   '/experiences',
