c00fb92cf9c160f5bd9e4f6b8a2b1c6c5e219326
kazaneshimizu <ks@rocketa.co.jp>
2025-03-13 19:59:25 +0900

    refactor: nullable picture

--- templates/nextjs-blog-starter/src/app/[lang]/_components/avatar.tsx
+++ templates/nextjs-blog-starter/src/app/[lang]/_components/avatar.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      {picture && <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />}
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
